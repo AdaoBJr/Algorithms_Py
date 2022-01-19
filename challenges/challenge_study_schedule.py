72715e0c33af0acd6394cc1fee05483efb655fe6
jaarguello <johnny.arguello007@gmail.com>
2022-01-19 16:29:10 -0300

    testing

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,21 +1,22 @@
+
+
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    x = 0
-    for time in permanence_period:
-        type1 = type(time[0]) is not int
-        type2 = type(time[1]) is not int
-        if type1 or type2 or target_time is None:
-            return None
+    try:
+        x = 0
+        for t in permanence_period:
+            ta = t[0]
+            tb = t[1]
+            e = ta == target_time or tb == target_time
+            c = ta <= target_time and tb >= target_time
 
-        equal = time[0] == target_time or time[1] == target_time
-        bigger_or_less = time[0] <= target_time and time[1] >= target_time
+            if e is True or c is True:
+                x += 1
+            
+        return x
 
-        if bigger_or_less is True or equal is True:
-            x += 1
-
-    if x == 0:
+    except TypeError:
         return None
-    return x
 
 
 study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2)
