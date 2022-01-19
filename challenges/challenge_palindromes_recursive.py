047ddb13b2b31d3ea7a63a634183be046c70bc73
Cristian Bugs <cristian.bugs@gmail.com>
2022-01-19 18:51:05 -0300

    second task

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,3 @@
-from pickle import FALSE
 def is_palindrome_recursive(word, low_index, high_index):
     if word == "":
         return False
@@ -7,14 +6,8 @@ def is_palindrome_recursive(word, low_index, high_index):
         print("entrou aqui")
         return False
 
-    if high_index  > low_index:
+    if high_index > low_index:
         low_index += 1
-        high_index -= 1 
+        high_index -= 1
         return is_palindrome_recursive(word, low_index, high_index)
     return True
-    # for i in range(0, len(word)-1):
-    #     string += word[-i]
-    # return string
-    
-
-print(is_palindrome_recursive("I", 0, len("i")-1))
