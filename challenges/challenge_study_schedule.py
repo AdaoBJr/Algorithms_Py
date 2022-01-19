01ad2e674566aca540333511bf74170fe395b50c
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2022-01-19 16:53:35 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    students_count = 0
 
+    if type(target_time) is not int:
+        return None
 
-# cometario para commit
+    for time in permanence_period:
+        if type(time[0]) is not int or type(time[1]) is not int:
+            return None
+        if time[0] <= target_time and time[1] >= target_time:
+            students_count += 1
+
+    return students_count
