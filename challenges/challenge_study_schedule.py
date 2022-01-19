c714fd8b4ae76c164af8352f896d308f7eda523c
Angelo Bittencourt <angelogpbl.info@gmail.com>
2022-01-19 16:55:00 -0300

    adicionando retorno none as entradas invalidas

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    if target_time == None:
+        return None
+    count = 0
+    for period in permanence_period:
+        if not all(period):
+            return None
+        if target_time in period:
+            count += 1
+    return count
