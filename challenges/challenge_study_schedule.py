0b124b1d1888309c3c054494c365e52957379655
Huggo Parcelly <h.parcelly@gmail.com>
2022-01-19 16:05:10 -0300

    adiciona a funcao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == 0 or type(target_time) != int:
+        return None
+    else:
+        all_permanence = []
+        # fazendo o desempacotamento de tuplas, fonte:
+        # https://pythonhelp.wordpress.com/2013/01/10/desempacotamento-de-tupla/
+        for entrada, saida in permanence_period:
+            if type(entrada) != int or type(saida) != int:
+                return None
+            all_permanence.extend(range(entrada, saida+1))
+                
+        return all_permanence.count(target_time)
