480c2cb0f12518fbf9890186d5ce8b2b47cffcde
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2022-01-19 16:14:13 -0300

    req. 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word or word[0] != word[-1]:
+        return False
+    if len(word) <= 1 or (len(word) == 2 and word[0] == word[1]):
+        return True
+    return is_palindrome_recursive(word[1:-1], word[0], word[-1])
+
+
+# https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
+
+# If a string has the first and last letters the same, and the remaining
+# letters ([1:-1]) are a palindrome, it's a palindrome.
