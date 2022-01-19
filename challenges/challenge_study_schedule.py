5a8ecfad21f1db8ec66ef56b5dd6c55c012d794b
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2022-01-19 15:15:59 -0300

    req. 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    result = 0
+    if target_time is None:
+        return None
+    for entrada, saida in permanence_period:
+        if entrada is None or saida is None:
+            return None
+        if entrada <= target_time <= saida:
+            result += 1
+    return result
