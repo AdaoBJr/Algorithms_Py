f05f81575fd15ebf2fb5e4e6d9e325251519a4a6
Diegho <dieghonm@gmail.com>
2022-01-19 16:39:14 -0300

    Requisito 1, tentativa 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    time = [0, 1, 2, 3, 4, 5, 6, 7]
-    timeCont = {}
-    for n in time:
-        timeCont[n] = 0
+    if type(target_time) != int:
+        return None
+    time = [0, 1, 2, 3, 4, 5]
+    timeCont = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
+
     for tupla in permanence_period:
+        if type(tupla[0]) != int or type(tupla[1]) != int:
+            return None
         for n in time[tupla[0]:tupla[1] + 1]:
             timeCont[n] += 1
     return timeCont[target_time]
+
+
+
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_period, 4))
