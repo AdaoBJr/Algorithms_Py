2fcc067711a32fcf71c22ae0218bf5dd4c7516e2
Renato Graça <renatomacedo1995@gmail.com>
2022-01-19 19:39:48 -0300

    finalmente um facil

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    low_index = 0
+    high_index = len(word) - 1
+    if len(word) < 2:
+        return True
+    for w in word:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
