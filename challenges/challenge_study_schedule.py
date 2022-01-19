15f34b57540988159a9eede1f11250c15ba95fd9
Paulo Xavier <pxavierf@gmail.com>
2022-01-19 14:31:12 -0300

    initial commit

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    return target_time
+
+
+if __name__ == '__main__':
+    permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+    print(study_schedule(permanence_period, 5))
