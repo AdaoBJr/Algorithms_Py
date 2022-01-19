30554b8bb5db4ff620414f419484a96fff3e3714
Mariana Mohr <marianamohr@gmail.com>
2022-01-19 17:12:07 -0300

    req 2 - feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # testa se a palavra é vazia
+    if word == '':
+        return False
+    # testa se os index passados maior ou igual
+    if low_index >= high_index:
+        return True
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
