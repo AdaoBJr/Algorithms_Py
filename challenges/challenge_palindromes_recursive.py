250e67027415fb172e2ed76e9505dc8771306864
Antenor <antenorzapata@gmail.com>
2022-01-19 16:24:28 -0300

    fix: flaker correction

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,9 @@
 def checkPalindrome(word):
-  if len(word) != 1:
-      result = checkPalindrome(word[1:])+word[0]
-      return result
-  else:
-      return word
+		if len(word) != 1:
+				result = checkPalindrome(word[1:])+word[0]
+				return result
+		else:
+				return word
 
 
 def is_palindrome_recursive(word, low_index, high_index):
