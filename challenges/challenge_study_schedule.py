c1a187c6a9ec1e62775d6a997dccb7fa047fb1c4
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-19 16:57:22 -0300

    Req01 , fazendo laço For para achar quantidade de pessoas simuntania em determinado horario

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    if type(target_time) is not int:
+        return None
+    for (entrada, saida) in permanence_period:
+        if type(entrada) is not int or type(saida) is not int:
+            return None
+        if entrada <= target_time and saida >= target_time:
+            count += 1
+    return count
