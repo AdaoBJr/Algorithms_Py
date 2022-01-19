b680f0a61a12481c36ab2ac48b5c08cead1db465
jaarguello <johnny.arguello007@gmail.com>
2022-01-19 17:10:11 -0300

    voltando o algoritimo para a versao anterior 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -9,13 +9,13 @@ def study_schedule(permanence_period, target_time):
             tb = t[1]
             c = ta <= target_time and tb >= target_time
 
-            if  c is True:
+            if c is True:
                 x += 1
 
         return x
 
     except TypeError:
-        return None 
+        return None
 
 
 study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2)
