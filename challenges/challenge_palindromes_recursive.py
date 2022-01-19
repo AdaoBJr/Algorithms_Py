17137b2b8679f1741fe7f9b326fa8af8dd3b4ff0
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2022-01-19 17:11:23 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    palindrome = False
+    if word == "":
+        return palindrome
+    if low_index >= high_index:
+        palindrome = True
+    elif word[low_index] == word[high_index]:
+        palindrome = is_palindrome_recursive(
+            word, low_index + 1, high_index - 1
+        )
+    return palindrome
