9775646c9214fe6cdf4be4a6902872523c152498
Antenor <antenorzapata@gmail.com>
2022-01-19 18:55:12 -0300

    complexity change

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,7 +8,7 @@ def study_schedule(permanence_period, target_time):
     for student in permanence_period:
         start = student[0]
         end = student[1]
-        if type(start) != int or type(end) != int:
+        if type(start) is not int or type(end) is not int:
             return None
         # if(target_time in range(start, end))
         if(target_time >= start and target_time <= end):
