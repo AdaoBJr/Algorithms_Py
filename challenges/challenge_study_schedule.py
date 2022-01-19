b8b5a44c6f062231872f5fd8df52e19904e89e62
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-19 17:25:22 -0300

    function study_schedule implemented

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    people_studying = 0
+
+    if target_time is None:
+        return None
+
+    for (start_study, finish_study) in permanence_period:
+        if type(start_study) is not int or type(finish_study) is not int:
+            return None
+
+        if start_study <= target_time <= finish_study:
+            people_studying += 1
+
+    return people_studying
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
