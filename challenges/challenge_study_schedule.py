338db4224d5e7297fc9f98d8eb6808bd1f442f89
hugolfsimoes <hugolfsimoes@gmail.com>
2022-01-19 16:38:54 -0300

    [X] 1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca) / menos teste 4

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for value in permanence_period:
+        if type(value[0]) != int or type(value[1]) != int:
+            return None
+        for index in range(value[0], value[1] + 1):
+            dict[index] += 1
+    return dict[target_time]
