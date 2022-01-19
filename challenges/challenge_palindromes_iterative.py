7a880aaeed83274d8947e3de99ece49d60cf95a7
Huggo Parcelly <h.parcelly@gmail.com>
2022-01-19 18:40:10 -0300

    adiciona a funçao palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    inverso = ""
+    for i in range(len(word) - 1, -1, -1):
+        inverso += word[i]
+    if word == inverso:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+
+    assert is_palindrome_iterative("huggo") is False
+    assert is_palindrome_iterative("") is False
+    assert is_palindrome_iterative("ana") is True
+    assert is_palindrome_iterative("socos") is True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    else:
+        if low_index in range(len(word) + 1) and high_index in range(
+            len(word) + 1
+        ):
+            if word[high_index - 1] != word[low_index - 1]:
+                return False
+
+            high_index -= 1
+            low_index += 1
+            is_palindrome_recursive(word, low_index, high_index)
+
+    return True
+
+
+if __name__ == "__main__":
+    palavra = "huggo"
+    print(is_palindrome_recursive(palavra, 0, len(palavra)))
