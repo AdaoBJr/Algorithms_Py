c681eb7fb8572934bfa38a34b72ad479bbf59b75
Mariana Mohr <marianamohr@gmail.com>
2022-01-19 16:54:33 -0300

    req 1 - feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. teste """
+    # testabto se o target_time é int
+    if type(target_time) is not int:
+        return None
+
+    count = 0
+    for item in permanence_period:
+        # testabto se os valores do periodo são int
+        if type(item[0]) is not int or type(item[1]) is not int:
+            return None
+        # testabto se o valor está dentro do range
+        if item[0] <= target_time and item[1] >= target_time:
+            count = count + 1
+    return count
