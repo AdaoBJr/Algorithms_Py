68fcbafc1eb55a133728c73bb203ff2ecaa88cdd
Angelo Bittencourt <angelogpbl.info@gmail.com>
2022-01-19 16:59:43 -0300

    verifica que o time esta entre os horarios

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,9 +3,10 @@ def study_schedule(permanence_period, target_time):
     if target_time == None:
         return None
     count = 0
+
     for period in permanence_period:
         if not all(period):
             return None
-        if target_time in period:
+        if target_time >= period[0] and target_time <= period[1]:
             count += 1
     return count
