19513339bfcd6eb734b0fc8334822339dc71da25
jaarguello <johnny.arguello007@gmail.com>
2022-01-19 19:45:14 -0300

    requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
+def palindrome(word, low_index, high_index):
+    if high_index == 0:
+        return word
+    else:
+        next = palindrome(word[:high_index], low_index + 1, high_index - 1)
+        return word[high_index] + next
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word == '':
+        return False
+    word1 = palindrome(word, low_index, high_index)
+    if word1 == word:
+        return True
+    return False
+
+
+# is_palindrome_recursive(word, 0, len(word) - 1)
+# print(is_palindrome_recursive(word, 0, len(word) - 1))
