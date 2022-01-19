7fab6e92bde8e970ef61f60efa4554eaa7e1bc65
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-19 15:33:24 -0300

    first attempt to resolve first requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is not None:
+        students = 0
+        for period in permanence_period:
+            if type(period[0]) != int or type(period[1]) != int:
+                return None
+            if target_time in range(period[0], period[1] + 1):
+                students += 1
+        return students
+    return None
