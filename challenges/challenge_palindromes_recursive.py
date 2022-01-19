7ce098f4f2fe9a2d1fb74568d8974abaf129810a
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-19 20:02:49 -0300

    implementing is_palindrome_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
+# for my reverse count https://stackoverflow.com/questions/869885/loop-backwards-using-indices-in-python
+
+def make_palindrome(word, low_index, high_index):
+    palindrome = ''
+    if high_index == low_index:
+        return palindrome + word[high_index]
+    else:
+        palindrome += word[high_index]
+        return palindrome + make_palindrome(word, low_index, high_index - 1)
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    return word == make_palindrome(word, low_index, high_index)
