9b1765bee44e93c035d8ec4e1c836d8668626f41
Renato Graça <renatomacedo1995@gmail.com>
2022-01-19 17:23:18 -0300

    requisito 1 izi

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    count = 0
+    for p in permanence_period:
+        if not p[0] or not p[1] or p[0] != int(p[0]) or p[1] != int(p[1]):
+            return None
+        if p[0] <= target_time and p[1] >= target_time:
+            count += 1
+
+    return count
