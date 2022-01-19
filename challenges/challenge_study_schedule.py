71fa1769ddd47d0811bfa86794009b9cbac6c5f7
Leticia Galvao <leticia.galvao@take.net>
2022-01-19 18:05:23 -0300

    Requisito 1 - Teste

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,20 +1,28 @@
-from asyncio.windows_events import NULL
+# def study_schedule(permanence_period, target_time):
+#     students_by_period = {}
+
+#     for tupla in permanence_period:
+#         if type(tupla[0]) != int or type(tupla[1]) != int or target_time is None:
+#             return None
+#         for i in range(tupla[0], tupla[1] + 1):
+#             if i in students_by_period:
+#                 students_by_period[i] += 1
+#             else:
+#                 students_by_period[i] = 1
+
+#     return students_by_period[target_time]
 
 
 def study_schedule(permanence_period, target_time):
-    students_by_period = {}
-    if target_time == None:
-        return None
+    t = 0
 
     for tupla in permanence_period:
-        if type(tupla[0]) != int or type(tupla[1]) != int:
+        
+        if type(tupla[0]) != int or type(tupla[1]) != int or target_time is None:
             return None
-        for i in range(tupla[0], tupla[1] + 1):
-            if i in students_by_period:
-                students_by_period[i] += 1
-            else:
-                students_by_period[i] = 1
+        else:
+            t += tupla.count(target_time)
+    return t
+  
 
-    return students_by_period[target_time]
-
-print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], None))
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 4))
