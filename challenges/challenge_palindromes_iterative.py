7e8b4ab389c56033b1dc9f2cb49a73f5238f8663
hugolfsimoes <hugolfsimoes@gmail.com>
2022-01-19 18:57:30 -0300

    [X] 2 - Palíndromos (Recursividade)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    word_reverse = ''
+    word_size = len(word) - 1
+    while(word_size >= 0):
+        word_reverse += word[word_size]
+        word_size -= 1
+    return word_reverse == word
