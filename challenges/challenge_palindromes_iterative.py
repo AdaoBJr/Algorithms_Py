cba862607d431799187bdd8a18140dd16acbaf9c
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-19 19:39:27 -0300

    Req02 e Req05 Verificando se a palavra e palidrome , iterative e recurecursive

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '': 
+        return False
+    reverse_word = word[::-1]
+    if reverse_word == word:
+        return True
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, low_index, high_index):  
+    if word == '':
+        return False
+    if len(word) == 1:
+        return True
+    if len(word) >= 2:
+        if word[low_index] == word[high_index]:
+            if len(word) == 2:
+                return True
+            return is_palindrome_recursive(word[1:-1], 0, -1)
+        return False
+
+
+word = "gg"
+print(is_palindrome_recursive(word, 0, len(word) - 1))
