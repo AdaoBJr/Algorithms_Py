1b3adf97422e50091dc880f16fde6dc086d1a671
Antenor <antenorzapata@gmail.com>
2022-01-19 16:51:29 -0300

    flake inacreditavel

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,13 @@
 def checkPalindrome(word):
     if(len(word) != 1):
-      result = checkPalindrome(word[1:]) + word[0]
-      return result
+        result = checkPalindrome(word[1:]) + word[0]
+        return result
     else:
-      return word
+        return word
 
 
 def is_palindrome_recursive(word, low_index, high_index):
     if bool(word) is False:
-      return False
+        return False
     isPali = checkPalindrome(word)
     return isPali == word
