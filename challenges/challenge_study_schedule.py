79ad68775d20165476fa45e26dea01a508f3f46e
Huggo Parcelly <h.parcelly@gmail.com>
2022-01-19 16:58:26 -0300

    correçao do tempo de execuçao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,12 +2,14 @@ def study_schedule(permanence_period, target_time):
     if target_time == 0 or type(target_time) != int:
         return None
     else:
-        all_permanence = []
+        count = 0
         # fazendo o desempacotamento de tuplas, fonte:
         # https://pythonhelp.wordpress.com/2013/01/10/desempacotamento-de-tupla/
         for entrada, saida in permanence_period:
             if type(entrada) != int or type(saida) != int:
                 return None
-            all_permanence.extend(range(entrada, saida + 1))
+            # Feito ajuda de Gabriel Essênio
+            if entrada <= target_time and saida >= target_time:
+                count += 1
 
-        return all_permanence.count(target_time)
+        return count
