e166e396ffabba2300f6ac6a9e2b75f0ff71dfc8
Angelo Bittencourt <angelogpbl.info@gmail.com>
2022-01-19 19:19:09 -0300

    reduz tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,7 +5,7 @@ def study_schedule(permanence_period, target_time):
     count = 0
 
     for period in permanence_period:
-        if not all(period):
+        if not type(period[0]) == int or not type(period[1]) == int:
             return None
         if target_time >= period[0] and target_time <= period[1]:
             count += 1
