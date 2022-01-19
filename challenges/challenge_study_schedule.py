7f7c646bac0ed620bde448b10dba7ea3faa2ac4a
Angelo Bittencourt <angelogpbl.info@gmail.com>
2022-01-19 19:31:10 -0300

    tempo execuçao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    if target_time is None:
+    if type(target_time) is not int:
         return None
     count = 0
 
     for period in permanence_period:
-        if not type(period[0]) == int or not type(period[1]) == int:
+        if not all(period):
             return None
         if target_time >= period[0] and target_time <= period[1]:
             count += 1
