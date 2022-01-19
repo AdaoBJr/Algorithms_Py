d7de83a6cd59760f04f3adcc3a44e770ee622df3
Paulo Xavier <pxavierf@gmail.com>
2022-01-19 15:48:48 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,24 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    array_string = list(word)
+    reverse_word = "".join(inverse_recursive(array_string))
+
+    if reverse_word != word:
+        return False
+    return True
+
+
+def inverse_recursive(array_string):
+    if len(array_string) == 0:
+        return array_string
+    return [array_string[-1]] + inverse_recursive(
+        array_string[: len(array_string) - 1]
+    )
+
+
+if __name__ == "__main__":
+    word = "ANA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    word = "AGUA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
