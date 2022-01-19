a4f17689d069360700ae764296d4c35c7735c16a
Thiago Marchini <thiago@msn.com>
2022-01-19 18:25:28 -0300

    Requisito 3 - início

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,54 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui.: """
+    if first_string == '' or second_string == '':
+        return False
+    first_array = first_string.split()
+    second_array = second_string.split()
+    first_sorted = quicksort(first_array, 0, len(first_string) - 1)
+    second_sorted = quicksort(second_array, 0, len(second_string) - 1)
+    print(first_sorted, second_sorted)
+    if first_sorted == second_sorted:
+        return True
+    else:
+        return False
+
+
+def quicksort(array, low, high):
+    # caso base: se já atingiu a menor porção (1)
+    if len(array) == 1:
+        # retorne o array
+        return array
+
+    # os índices irão se cruzar quando o array estiver ordenado
+    if low < high:
+        # índice da partição é o índice onde o array foi divido
+        # e é determinado a partir do pivô.
+        # Este índice já está ordenado
+        partition_index = partition(array, low, high)
+
+        # Ordena os elementos presentes antes da partição (menores que o pivô)
+        # e depois (maiores que o pivô)
+        quicksort(array, low, partition_index - 1)
+        quicksort(array, partition_index + 1, high)
+
+
+# função auxiliar responsável pela partição do array
+# escolhendo um pivô e fazendo movimentações dos sub arrays gerados
+def partition(array, low, high):
+    # índice do menor elemento
+    i = low - 1
+    # o pivô será escolhido
+    # através do índice que divide o array
+    pivot = array[high]  # pivot
+
+    # itera sobre os elementos
+    for j in range(low, high):
+
+        # se o elemento corrente é menor ou igual ao pivô
+        if array[j] <= pivot:
+
+            # incrementa o índice do menor elemento
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
