427a76c81e1c0738395a42d97715be9537dc05eb
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2022-01-19 17:55:16 -0300

    limpando comentários

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -10,7 +10,3 @@ def study_schedule(permanence_period, target_time):
         return None
 
     return conter
-
-
-# result = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5)
-# print(result)
