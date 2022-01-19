a5cad97a75117830c6bef00df86ff69ddfe8fbcd
Cristian Bugs <cristian.bugs@gmail.com>
2022-01-19 19:23:49 -0300

    third task partially done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    second_word = ""
+
+    for i in range(len(second_string), 0, -1):
+        second_word += second_string[i-1]
+
+    if first_string == "":
+        return False
+
+    if first_string == second_word:
+        return True
+    
+    return False
+
+# print(is_anagram("pedra", "perda"))
