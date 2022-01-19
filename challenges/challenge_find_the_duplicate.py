99b25fb4fca0db54039097aab5c3fa54c16e5c15
hugolfsimoes <hugolfsimoes@gmail.com>
2022-01-19 19:55:51 -0300

    [X] 4 - Encontrando números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for num in nums:
+        cont2 = 0
+        cont = len(nums) - 1
+        if not nums or type(num) != int or len(nums) < 2 or num < 0:
+            return False
+        for index in range(len(nums)):
+            if num == nums[cont]:
+                cont2 += 1
+            cont -= 1
+        if cont2 > 1:
+            return num
+    return False
