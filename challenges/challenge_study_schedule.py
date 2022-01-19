9ec57d6d291d40f4b93a6437b2d60e472646a7f7
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2022-01-19 17:35:55 -0300

    refatorando rq 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    if not isinstance(target_time, int):
-        return None
-
     conter = 0
 
-    for tupla_period in permanence_period:
-        try:
-            if int(tupla_period[0]) <= target_time <= int(tupla_period[1]):
+    try:
+        target = int(target_time)
+        for tupla_period in permanence_period:
+            if int(tupla_period[0]) <= target <= int(tupla_period[1]):
                 conter += 1
-        except TypeError:
-            return None
+    except TypeError:
+        return None
 
     return conter
 
