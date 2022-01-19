60235a03c87d1a509cd91e82f081191d1f288d12
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2022-01-19 14:19:16 -0300

    req 2 = feito

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    primeira_letra = word[0]
+    ultima_letra = word[-1]
+    if primeira_letra != ultima_letra:
+        return False
+    elif len(word) <= 2:
+        return True
+    else:
+        return is_palindrome_iterative(word[1:-1])
