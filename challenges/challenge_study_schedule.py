40ac16df117810b30a26710efd984df7687d5cac
Dennis (Ubuntu) <dennismarcelo15@gmail.com>
2022-01-19 17:33:25 -0300

    removendo teste

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -14,5 +14,5 @@ def study_schedule(permanence_period, target_time):
     return conter
 
 
-result = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5)
-print(result)
+# result = study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5)
+# print(result)
