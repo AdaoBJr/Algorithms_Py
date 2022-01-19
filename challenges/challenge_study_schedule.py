f275571d7422b69fd075b043c9e85f56c805b3e5
Paulo Xavier <pxavierf@gmail.com>
2022-01-19 15:53:08 -0300

    lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,7 +7,7 @@ def study_schedule(permanence_period, target_time):
 
     count = 0
     for schedule in permanence_period:
-        if type(schedule[0]) != int or type(schedule[1]) != int: 
+        if type(schedule[0]) != int or type(schedule[1]) != int:
             return None
         if schedule[0] <= target_time <= schedule[1]:
             count += 1
@@ -18,3 +18,4 @@ def study_schedule(permanence_period, target_time):
 if __name__ == '__main__':
     permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
     print(study_schedule(permanence_period, 1))
+    
