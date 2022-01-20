d4bd1c9b138b33986d9b940431254bf48f471dd4
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-20 03:51:54 -0300

    teste com espaços vazios

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    # https://stackoverflow.com/questions
+    # /30340208/recursively-checking-if-a-list-is-a-palindrome-in-python
     if not word:
         return False
     if low_index == high_index or high_index < low_index:
@@ -8,6 +10,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     return (
         word[low_index] == word[high_index]) and (
             is_palindrome_recursive(word, low_index + 1, high_index - 1))
-
-# https://stackoverflow.com/questions
-# /30340208/recursively-checking-if-a-list-is-a-palindrome-in-python
