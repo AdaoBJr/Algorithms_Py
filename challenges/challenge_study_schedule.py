d415e246f889c152635400b5905ece0bda4339ee
Jonathan Souza <souzajonathan570@gmail.com>
2022-01-20 10:59:58 -0300

    req 01 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    i = 0
+    size = len(permanence_period)
+
+    if target_time is None:
+        return None
+
+    while (i < size):
+        if None in permanence_period[i]:
+            return None
+        elif permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+            count += 1            
+
+        i += 1
+
+    return count
