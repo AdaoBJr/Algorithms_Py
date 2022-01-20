1412becd1c86c1f15c926dca8361ad303650e876
isTorres <ismaeltorres612@gmail.com>
2022-01-20 17:18:05 -0300

    feat: palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    pass
-    # if word == '':
-    #     return False
-    # word[low_index] == word[high_index]
+    if word == '':
+        return False
+    if len(word) <= 2 and word[0] == word[-1]:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome_recursive(word[1:-1], low_index, high_index)
