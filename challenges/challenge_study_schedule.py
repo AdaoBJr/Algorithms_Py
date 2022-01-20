f6fd2922ce8c03383a9c7ea197524703741c49f4
Diego Serafim <diegoserafim1@gmail.com>
2022-01-19 22:19:16 -0300

    condicao login/logou null

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,6 +3,8 @@ def study_schedule(permanence_period, target_time):
     if type(target_time) is not int:
         return None
     for (login, logout) in permanence_period:
+        if type(login) is not int or type(logout) is not int:
+            return None
         if login <= target_time <= logout:
             students_online += 1
     return students_online
