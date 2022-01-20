ee3f0e65f6d1f87271d1587be5310b0583137107
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-20 03:32:16 -0300

    stack over show palindromes recursivos

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,10 +3,6 @@ def is_palindrome_iterative(word):
     # verificação de string vazia python "Gleison"
     if not word:
         return False
-    # i = len(word_list)
-    # string_reversed = ""
-    # while i > 0:
-    #     string_reversed += word_list[i-1]
-    #     i = i - 1
+    # Metodo pythonic gleison
 
     return word == word[::-1]
