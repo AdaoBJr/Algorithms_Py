00d1217f834c1d10ca1cdbe01fb6e65865ce8396
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-20 13:38:06 -0300

    requisito 1?? nao entendi ver no plantao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,9 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count = 0
     if not target_time:
-        return False
+        return None
+    for in_out in permanence_period:
+        if not in_out[0] or in_out[1]:
+            return None
+    return count
