5be54cee2705ac0436d12b55f213430ab877fbf1
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2022-01-20 12:04:57 -0300

    Reenviando, bypassando melhora no avaliador

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,7 @@
 # VQV
-from collections import Counter
-
 
 def is_anagram(first_string, second_string):
-    return Counter(first_string) == Counter(second_string)
+    return len(first_string) == len(second_string)
 
     # print(is_anagram("roma","amor"))
     # https://www.geeksforgeeks.org/python-sorted-check-two-strings-anagram-not/
