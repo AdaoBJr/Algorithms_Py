6a62104f492c41b506e24579a1ca46d700dd6ee7
Pedro Toselli <phtoselli@gmail.com>
2022-01-20 14:48:54 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    student = 0
+
+    if target_time == None:
+        return None
+
+    for (inicio, fim) in permanence_period:
+        # print(f"inicio: {inicio}, fim: {fim}")
+        if type(inicio) != int or type(fim) != int:
+            return None
+        if inicio <= target_time <= fim:
+        # if target_time in range(inicio, fim):
+            student += 1
+    return student
+
+
+# if __name__ == '__main__':
+#     print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 4))
