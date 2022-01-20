571d8f36ced4ce9989f16e420a3bb7f8824c927a
Diego Serafim <diegoserafim1@gmail.com>
2022-01-20 18:47:16 -0300

    anagrams sem o sort :/

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == second_string:
+        return True
+    if reversed(first_string) == reversed(second_string):
+        return True
+
+    return False
+        
+
+# s1 ="listen"
+# s2 ="silent" 
+# is_anagram(s1, s2) 
