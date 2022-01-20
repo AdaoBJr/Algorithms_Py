ffb3062b31eadc5edb722891bbd8afbcd02f646d
Pedro Toselli <phtoselli@gmail.com>
2022-01-20 15:02:18 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,10 +5,11 @@ def study_schedule(permanence_period, target_time):
         return None
 
     for (inicio, fim) in permanence_period:
-        # print(f"inicio: {inicio}, fim: {fim}")
         if type(inicio) != int or type(fim) != int:
             return None
         if inicio <= target_time <= fim:
-            # if target_time in range(inicio, fim):
             student += 1
     return student
+
+    # print(f"inicio: {inicio}, fim: {fim}")
+    # if target_time in range(inicio, fim):
