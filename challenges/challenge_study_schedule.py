117ab1a0cb6e12c078d8706dbecef39025f8a7a4
Diego Serafim <diegoserafim1@gmail.com>
2022-01-19 22:11:42 -0300

    esquecia de criar a branch.. ficou na master, tentarei recuperar

--- challenges/challenge_study_schedule.py
+++
@@ -1,8 +0,0 @@
-def study_schedule(permanence_period, target_time):
-    students_online = 0
-    if type(target_time) is not int:
-        return None
-    for (login, logout) in permanence_period:
-        if login <= target_time <= logout:
-            students_online += 1
-    return students_online
