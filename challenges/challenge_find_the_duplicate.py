c4d94917b357ca2b1eac248041de8bd5295798fd
andersontrkz <trkz.anderson@gmail.com>
2022-01-20 18:57:52 -0300

    feat: challenge find duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    length = len(nums)
+
+    nums.sort()
+
+    for index in range(length - 1):
+        if type(nums[index]) != int or nums[index] < 0:
+            return False
+
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
