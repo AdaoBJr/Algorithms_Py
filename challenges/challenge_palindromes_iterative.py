8f3c13cf1f01e71e8fb27b007200f5b9492a8ead
Renato Graça <renatomacedo1995@gmail.com>
2022-01-20 18:55:40 -0300

    req 5 em 3 linhas

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,13 +1,7 @@
 def is_palindrome_iterative(word):
     if word == '':
         return False
-    low_index = 0
-    high_index = len(word) - 1
-    if len(word) < 2:
+    word_invert = word[::-1]
+    if word_invert == word:
         return True
-    for w in word:
-        if word[low_index] != word[high_index]:
-            return False
-        low_index += 1
-        high_index -= 1
-    return True
+    return False
