0a94cf36b540df2e8a57d9f5b2ee0f8d0803ca4e
Leticia Galvao <leticia.galvao@take.net>
2022-01-20 19:15:01 -0300

    Requisito 4 - Funcao find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
+# Source https://www.horadecodar.com.br/2020/08/06/como-contar-
+# o-numero-de-ocorrencias-em-uma-list-em-python/
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 1 or (len(nums) == 2 and nums[0] != nums[1]):
+        return False
+
+    for n in nums:
+        if type(n) is str or n < 0:
+            return False
+        if nums.count(n) > 1:
+            return n
+
+# nums = [1, 3, 4, 2, 2]
+# print(find_duplicate(nums))
