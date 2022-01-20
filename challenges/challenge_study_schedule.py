db7debb07ec863427868b37ae7227122abcfca77
Jonathan Souza <souzajonathan570@gmail.com>
2022-01-20 11:58:53 -0300

    req 01 refatorado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,14 @@
 def study_schedule(permanence_period, target_time):
     count = 0
-    i = 0
-    size = len(permanence_period)
 
     if target_time is None:
         return None
 
-    while (i < size):
-        if None in permanence_period[i]:
+    for period in permanence_period:
+        if None in period:
             return None
-        elif permanence_period[i][0] <= target_time <= permanence_period[i][1]:
-            count += 1            
 
-        i += 1
+        if period[0] <= target_time <= period[1]:
+            count += 1
 
     return count
