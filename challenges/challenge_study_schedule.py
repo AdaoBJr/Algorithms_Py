4dc9dffada38cacb05dd62c646c3b1d1a305a4b1
Gabriel <gabriel.mirandab92@gmail.com>
2022-01-19 21:06:16 -0300

    adding `study_schedule` function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    hour = 0
+    for p in permanence_period:
+        if type(p[0]) is not int or type(p[1]) is not int:
+            return None
+
+        if p[0] <= target_time <= p[1]:
+            hour += 1
+
+    return hour
