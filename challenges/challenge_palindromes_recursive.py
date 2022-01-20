20e318316f4e4b9f6fc4c52a879310123e1243d9
Raphael Gumieri <raphaelgumieri@hotmail.com>
2022-01-20 18:25:25 -0300

    feat: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,3 +2,13 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word == '':
         return False
     
+    if len(word) == 1:
+        return True
+
+    if word[low_index] == word[high_index]:
+        cut_word = word[1:-1]
+        if len(cut_word) == 0:
+            return True
+        return is_palindrome_recursive(cut_word, 0, -1)
+
+    return False
