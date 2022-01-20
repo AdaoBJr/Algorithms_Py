8c35b86eb65f6ca945358d3cc0a71459bdcf8c4a
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-20 17:06:56 -0300

    Fazendo sorte logn(n),  e comparando strings

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,28 +1,48 @@
 def is_anagram(first_string, second_string):
-    first_string_sorted = sorted_string(first_string)
-    second_string_sorted = sorted_string(second_string)
-    print(first_string_sorted)
-    print(second_string_sorted)
-
-    if len(first_string_sorted) != len(second_string_sorted):
-        return False
-    
-    if first_string_sorted == second_string_sorted:
+    first_sorted = merge_sort(list(first_string))
+    second_sorted = merge_sort(list(second_string))
+    print(first_sorted)
+    print(second_sorted)
+    first_string_sorted = "".join(first_sorted)
+    second_string_sorted = "".join(second_sorted)
+    if first_string_sorted == second_string_sorted and len(
+        first_string_sorted
+    ) == len(second_string_sorted):
         return True
-       
-
-def sorted_string(string):
-    list_string = list(string)
-
-    for i in range(len(list_string)):
-        curr_value = list_string[i]
-        curr_index = i
-
-        while curr_index > 0 and list_string[curr_index - 1] > curr_value:
-            list_string[curr_index] = list_string[curr_index - 1]
-            curr_index = curr_index - 1
-        list_string[curr_index] = curr_value
-    return "".join(list_string)
+    return False
 
 
-print(is_anagram("amqqwoqr", "romqqqwa"))
+# SORTED RETIRADO DO COURSE DA TRYBE
+def merge_sort(array):
+
+    if len(array) <= 1:
+
+        return array
+
+    mid = len(array) // 2
+
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+
+        merged[left_cursor + right_cursor] = right[right_cursor]
+        right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
