16436cfc94d0556a4930c16711b865273f790d5f
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-20 13:59:27 -0300

    requisito study_shedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,19 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    count = 0
     if not target_time:
         return None
     for in_out in permanence_period:
-        if not in_out[0] or in_out[1]:
+        if (in_out[0] is None) or (in_out[1] is None):
             return None
+
+    return count_only(permanence_period, target_time)
+
+
+# https://www.geeksforgeeks.org/10-essential-python-tips-tricks-programmers/
+# Tricks é palavra pra te ajudar se um monstro em python
+def count_only(permanence_period, target_time):
+    count = 0
+    for in_out in permanence_period:
+        if in_out[0] <= target_time <= in_out[1]:
+            count += 1
     return count
