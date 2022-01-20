e4ca51210e06f96fae7ccc69c096d66e231009dc
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-20 00:28:11 -0300

    Resolves anagrams challenge

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,24 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string != "" and second_string != "":
+        first_string_in_order = order_string(first_string)
+        second_string_in_order = order_string(second_string)
+        if first_string_in_order == second_string_in_order:
+            return True
+        return False
+    return False
+
+
+# Inspirado no course, algoritmo de ordernação (inserção)
+def order_string(string_to_order):
+    string_list = list(string_to_order)
+    for i in range(len(string_list)):
+        current_value = string_list[i]
+        current_position = i
+        while (
+            current_position > 0
+            and string_list[current_position - 1] > current_value
+        ):
+            string_list[current_position] = string_list[current_position - 1]
+            current_position = current_position - 1
+        string_list[current_position] = current_value
+    return "".join(string_list)
