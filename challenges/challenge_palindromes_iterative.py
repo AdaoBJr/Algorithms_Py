8a710893bfea72768af9056e2294d7d2a2b57821
Leticia Galvao <leticia.galvao@take.net>
2022-01-20 15:16:32 -0300

    Requisito 5 - Palíndromos (Iteratividade)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
+# Source https://www.portugal-a-programar.pt/forums/topic/72774
+# -pal%C3%ADndrome-em-python-funcional/
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    for a, b in zip(word, reversed(word)):
+        if not a == b:
+            return False
+    return True
