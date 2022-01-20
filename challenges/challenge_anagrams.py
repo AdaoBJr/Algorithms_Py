65714608c284b1aaf369c82f22506a4c292879c6
Raphael Gumieri <raphaelgumieri@hotmail.com>
2022-01-20 19:12:02 -0300

    fix: lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -39,4 +39,4 @@ def is_anagram(first_string, second_string):
     return list1 == list2
 
 
-# merge_sorte e merge retiradas de: https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+# merge_sorte e merge retiradas do course
