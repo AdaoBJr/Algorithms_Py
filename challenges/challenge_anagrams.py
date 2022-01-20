3710cdc70169df5611ec844f3a4e73287b471282
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-20 14:31:26 -0300

    correcting is_anagram function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,33 +1,41 @@
-def partition(str, low_index, high_index):
-    i = low_index - 1
-    pivot = str[high_index]
-    for j in range(low_index, high_index):
-        if str[j] <= pivot:
-            i = i + 1
-            str[i], str[j] = str[j], str[i]
-    str[i + 1], str[high_index] = str[high_index], str[i + 1]
+def partition(arr, low_index, high_index):
+    pivot = arr[high_index]
+    item = low_index - 1
 
-    return i + 1
+    for index in range(low_index, high_index):
+        if arr[index] <= pivot:
+            item = item + 1
+            (arr[item], arr[index]) = (arr[index], arr[item])
+
+    arr[item + 1], arr[high_index] = arr[high_index], arr[item + 1]
+
+    return item + 1
 
 
 # I'll try a quick sort :D
-# btw, the course helped me to remember the function
-def sort_string(str, low_index, high_index):
-    if len(str) == 1:
-        return str
-
+# this site helped me https://careerkarma.com/blog/python-quick-sort
+def sort_string(arr, low_index, high_index):
     if low_index < high_index:
-        partition_index = partition(str, low_index, high_index)
+        pivot = partition(arr, low_index, high_index)
 
-        sort_string(str, low_index, partition_index - 1)
-        sort_string(str, partition_index + 1, high_index)
+        sort_string(arr, low_index, pivot - 1)
+
+        sort_string(arr, pivot + 1, high_index)
 
 
 def is_anagram(first_string, second_string):
-    first_str_to_arr = first_string.split()
-    second_str_to_arr = second_string.split()
+    first_str_to_arr = list(first_string)
+    second_str_to_arr = list(second_string)
+
     first_length = len(first_str_to_arr) - 1
     secong_length = len(second_str_to_arr) - 1
-    first_str_sorted = sort_string(first_str_to_arr, 0, first_length)
-    second_str_sorted = sort_string(second_str_to_arr, 0, secong_length)
-    return (first_str_sorted == second_str_sorted)
+
+    sort_string(first_str_to_arr, 0, first_length)
+    sort_string(second_str_to_arr, 0, secong_length)
+
+    separator = ''
+
+    first_str_sorted = separator.join(first_str_to_arr)
+    second_str_sorted = separator.join(second_str_to_arr)
+
+    return first_str_sorted == second_str_sorted
