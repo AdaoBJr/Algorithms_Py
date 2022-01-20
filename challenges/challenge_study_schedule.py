85a203dbb8c69517377a7b161da921cf62f910fb
isTorres <ismaeltorres612@gmail.com>
2022-01-20 15:44:55 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_online = 0
+    if type(permanence_period) != list or type(target_time) != int:
+        return None
+    else:
+        for item in permanence_period:
+            if item[0] == None or item[1] == None:
+                return None
+            elif target_time >= item[0] and target_time <= item[1]:
+                students_online += 1
+        return students_online    
