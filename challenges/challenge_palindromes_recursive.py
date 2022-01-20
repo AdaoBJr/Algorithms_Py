d4388644da3503e9dcc13571123996d7592b6631
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-20 00:27:44 -0300

    Create palindromes_recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # Testa se a palavra é nula ou difere o primeiro do último caracter
+    if len(word) <= 0 or word[low_index] != word[high_index]:
+        return False
+    # Testa se a palavra após passar na checagem acima
+    # tem dois ou mais caracteres o que indica que é um palindromo
+    if len(word) <= 2:
+        return True
+    # Envia para a próxima chamada a própria função a palavra
+    # sem o primeiro e último elemento até que passe em alguma verificação
+    midst_word = word[1:-1]
+    return is_palindrome_recursive(midst_word, 0, len(midst_word) - 1)
