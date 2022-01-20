0240cb8d12e752e4b44b8f162f41f763e4967332
Angelo Bittencourt <angelogpbl.info@gmail.com>
2022-01-20 00:22:44 -0300

    requisito 3 começo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
+    n = 0
+    if first_string == "" or second_string == "":
+        return False
+    for letra in second_string:
+        primeira = first_string[n]
+        if letra == primeira:
+            n += 1
+        else:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    is_anagram("pedra", "perda")
