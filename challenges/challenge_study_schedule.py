4dff42d7151b0181bc0e02727eab841a6aaeee27
Raphael Gumieri <raphaelgumieri@hotmail.com>
2022-01-20 18:04:22 -0300

    feat: study_schedule upgrade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
     result = 0
-    if type(target_time) is not int:
+    if not isinstance(target_time, int):
         return None
 
     for time in permanence_period:
-        if type(time[0]) is not int or type(time[1]) is not int:
+        if not isinstance(time[0], int) or not isinstance(time[1], int):
             return None
         if time[0] <= target_time and time[1] >= target_time:
             result += 1
