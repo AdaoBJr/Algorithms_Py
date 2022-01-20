225f7dc0270529132c71c5d06a691f4bc6e350d4
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2022-01-20 18:26:17 -0300

    req 5, feito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,23 @@
+# fiz em parceria com o Pedro Henrique Pires do Nascimento
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if "" in nums or not nums or len(nums) == 1:
+        return False
+    if len(nums) <= 2 and nums[0] != nums[1]:
+        return False
+    result = finde(nums, 0, 0)
+
+    return result
+
+
+def finde(nums, count, value):
+    if len(nums) == 1:
+        return value
+    for n in nums[1:]:
+        # print(n, nums[0])
+        if not isinstance(nums[0], int) or nums[0] <= 0:
+            return False
+        if nums[0] == n and nums[0] > value:
+            count += 1
+            value = n
+            return finde(nums[1:], count, value)
+    return finde(nums[1:], count, value)
