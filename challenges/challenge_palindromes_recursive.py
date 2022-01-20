3066bf5c0116650acba72460a1d9f9f181b40485
Diegho <dieghonm@gmail.com>
2022-01-20 00:46:46 -0300

    Requisito 2, tentativa 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,11 +9,4 @@ def is_palindrome_recursive(word, low_index, high_index):
     
     return is_palindrome_recursive(word[1:length], 0, 0)
 
-print(is_palindrome_recursive("GG",'', ''))
-print(is_palindrome_recursive('A', '', ''))
-print(is_palindrome_recursive("ANA",'', ''))
-print(is_palindrome_recursive("SOCOS",'', ''))
-print(is_palindrome_recursive("REVIVER",'', ''))
-print(is_palindrome_recursive("AGUA",'', ''))
-print(is_palindrome_recursive("COXINHA",'', ''))
-print(is_palindrome_recursive('', '', ''))
+    
