9c528fa0cf9f1073640e8c4cea39397b0940397e
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-20 05:33:18 -0300

    Refatorando palindrome recursive, diminuindo complexidade da funcao

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,15 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):  
-    if word == '':
+    if not word:
         return False
     if len(word) == 1:
         return True
-    if len(word) >= 2:
-        if word[low_index] == word[high_index]:
-            if len(word) == 2:
-                return True
-            return is_palindrome_recursive(word[1:-1], 0, -1)
-        return False
-
-
-word = "gg"
-print(is_palindrome_recursive(word, 0, len(word) - 1))
+    if word[low_index] == word[high_index]:
+        if len(word) <= 2:
+            return True
+        return is_palindrome_recursive(word[1:-1], 0, -1)
+    return False
