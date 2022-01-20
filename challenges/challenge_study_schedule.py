ea07465f231a2a43873cdf56b1b6637e05e4e863
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-20 00:25:23 -0300

    little refactor on study_schedule challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     if target_time is not None:
         students = 0
         for period in permanence_period:
-            if type(period[0]) != int or type(period[1]) != int:
+            if type(period[0]) is not int or type(period[1]) is not int:
                 return None
             if target_time in range(period[0], period[1] + 1):
                 students += 1
