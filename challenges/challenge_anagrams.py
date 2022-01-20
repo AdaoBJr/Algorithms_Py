d20dc5d7d524a646c30ddf912f2e1cbcef8f46c3
Gabriel Essênio <gabriel.essenio@gmail.com>
2022-01-20 06:18:09 -0300

    Tentando otimizar tempo do Req01 , Iniciando Req04 criando Sorted e comparando strings

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,28 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string_sorted = sorted_string(first_string)
+    second_string_sorted = sorted_string(second_string)
+    print(first_string_sorted)
+    print(second_string_sorted)
+
+    if len(first_string_sorted) != len(second_string_sorted):
+        return False
+    
+    if first_string_sorted == second_string_sorted:
+        return True
+       
+
+def sorted_string(string):
+    list_string = list(string)
+
+    for i in range(len(list_string)):
+        curr_value = list_string[i]
+        curr_index = i
+
+        while curr_index > 0 and list_string[curr_index - 1] > curr_value:
+            list_string[curr_index] = list_string[curr_index - 1]
+            curr_index = curr_index - 1
+        list_string[curr_index] = curr_value
+    return "".join(list_string)
+
+
+print(is_anagram("amqqwoqr", "romqqqwa"))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
     count = 0
-    if type(target_time) is not int:
+    if type(target_time) != int:
         return None
     for (entrada, saida) in permanence_period:
-        if type(entrada) is not int or type(saida) is not int:
+        if type(entrada) != int or type(saida) != int:
             return None
         if entrada <= target_time and saida >= target_time:
             count += 1
