42fe81f8f151c9e1b2853e92ed215827ba895a1b
andersontrkz <trkz.anderson@gmail.com>
2022-01-20 20:30:58 -0300

    feat: palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
+# BASED ON
+# https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if word[high_index] != word[low_index]:
+        return False
+
+    if high_index > low_index:
+        low_index += 1
+        high_index -= 1
+        return is_palindrome_recursive(word, low_index, high_index)
+
+    return True
