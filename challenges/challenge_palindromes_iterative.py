4053213bf3cf7e7573218981750ddc58a9e4d352
Leticia Galvao <leticia.galvao@take.net>
2022-01-20 19:36:31 -0300

    Requisito 5 - teste para ver se o tempo passa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,18 @@
 # Source https://www.portugal-a-programar.pt/forums/topic/72774
 # -pal%C3%ADndrome-em-python-funcional/
 
+# def is_palindrome_iterative(word):
+#     if word == '':
+#         return False
+
+#     for a, b in zip(word, reversed(word)):
+#         if not a == b:
+#             return False
+#     return True
+
+# https://www.horadecodar.com.br/2020/12/10/como-inverter-uma-string-em-python/
+
 def is_palindrome_iterative(word):
     if word == '':
         return False
-
-    for a, b in zip(word, reversed(word)):
-        if not a == b:
-            return False
-    return True
+    return word == word[::-1]
