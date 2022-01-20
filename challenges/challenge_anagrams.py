0ddf1c1994e991db7517eff3c27a92af833ae4f0
EderPaiva <edergomesdepaiva12@gmail.com>
2022-01-20 03:48:18 -0300

    lint esquisito

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,13 @@
+def quick_sort(lista):
+    for passnum in range(len(lista)-1, 0, -1):
+        for i in range(passnum):
+            if lista[i] > lista[i+1]:
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+    return lista
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     # Na tabela ascii cada letra possui um número e eles diferenciam
@@ -10,13 +20,3 @@ def is_anagram(first_string, second_string):
     first_string = quick_sort(first_string)
     second_string = quick_sort(second_string)
     return first_string == second_string
-
-
-def quick_sort(lista):
-    for passnum in range(len(lista)-1, 0, -1):
-        for i in range(passnum):
-            if lista[i] > lista[i+1]:
-                temp = lista[i]
-                lista[i] = lista[i+1]
-                lista[i+1] = temp
-    return lista
