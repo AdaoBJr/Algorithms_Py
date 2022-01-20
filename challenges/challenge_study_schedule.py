3913d0e29c22c60838b5aaa28c95837454a6c4ad
brunamfguimaraes <brunamfguimaraes@gmail.com>
2022-01-20 14:39:47 -0300

    Iniciando projeto - requisito 1 - ainda nao concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
+# https://blog.betrybe.com/tecnologia/tuplas-em-python/
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+
+    total = 0
+    for pp in permanence_period:
+
+        if type(pp[0]) is not int or type(pp[1]) is not int:
+            return None
+# https://www.devmedia.com.br/estruturas-de-condicao-em-python/37158
+        if pp[0] <= target_time and pp[1] >= target_time:
+            total = total + 1
+    return total
