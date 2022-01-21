8139bce1a21e3daf5742c9401699fe0126f055d1
Gabriel <gabriel.mirandab92@gmail.com>
2022-01-21 18:51:23 -0300

    adding `is_palindrome_recursive` function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
+# Adaptado do artigo:
+# https://acervolima.com/funcao-recursiva-para-verificar-se-uma-string-e-palindromo/
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    word = word.lower()
+
+    if word == "" or word[low_index] != word[high_index]:
+        return False
+
+    if len(word) < 2:
+        return True
+
+    if low_index < high_index + 1:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return True
