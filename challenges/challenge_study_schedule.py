908248eb555c6a829de886c9dab682fbe14ab7cb
Misael Bueno <misa.hbueno@gmail.com>
2022-01-21 14:54:40 -0300

    req1 test2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,9 @@ def study_schedule(permanence_period, target_time):
     for a, b in permanence_period:
         if type(target_time) != int or type(a) != int or type(b) != int:
             return None
-        for i in range(a, b+1):
-            if i == target_time:
-                count += 1
+        # for i in range(a, b+1):
+        #     if i == target_time:
+        #         count += 1
+        if target_time >= a and target_time <= b:
+            count += 1
     return count
