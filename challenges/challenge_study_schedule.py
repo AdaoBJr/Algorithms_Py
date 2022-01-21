e2f5a4e599c4ac22c855e198c4dab64cf571576d
antonioarieiro <sfgfffs@gmail.com>
2022-01-21 13:51:31 -0300

    fix lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    return permanence_period
+    if(type(target_time) != int):
+        return None
+    count = 0;
+    
+    for peooples in permanence_period:
+        if type(peooples) is not int:
+            return None
+    return count
