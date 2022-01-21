d6bd488205e762941626f5d24db2e41a41b7e7bf
Diego Serafim <diegoserafim1@gmail.com>
2022-01-20 22:22:11 -0300

    erro ~n compreendido

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -4,16 +4,12 @@ def find_duplicate(nums):
     for i in nums:
         if i not in duplicates:
             duplicates.append(i)
-
     for j in duplicates:
         if duplicates.count(j) > 1:
             if j not in repeat:
                 repeat.append(i)
-
     # repeat = {duplicates.count(item) for item in duplicates}
-
     if type(nums) != int or nums < 1:
         return False
 
-    # print(repeat)
-    return repeat
+    return repeat[0]
