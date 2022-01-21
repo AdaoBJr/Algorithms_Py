8ae65facff32699e509e533a469cee9f9cba3b53
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-21 08:53:18 -0300

    making find_duplicate with complexity O(n^2)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
+from unicodedata import numeric
+
+
 def find_duplicate(nums):
-    if nums is not None or len(nums) <= 2 or isinstance(nums, str):
+    if nums is None or len(nums) <= 2 or isinstance(nums, str):
         return False
+
+    for index, number in enumerate(nums):
+        for indexY, numberY in enumerate(nums):
+            if index != indexY and number == numberY:
+                return number
+
+    return False
