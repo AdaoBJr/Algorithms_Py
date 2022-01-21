52ab530891c6556c3d00517eb4572a5de5c93da2
Misael Bueno <misa.hbueno@gmail.com>
2022-01-21 15:09:24 -0300

    req1 test3

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if type(target_time) != int:
+        return None
     count = 0
     for a, b in permanence_period:
-        if type(target_time) != int or type(a) != int or type(b) != int:
+        if type(a) != int or type(b) != int:
             return None
-        # for i in range(a, b+1):
-        #     if i == target_time:
-        #         count += 1
         if target_time >= a and target_time <= b:
             count += 1
     return count
