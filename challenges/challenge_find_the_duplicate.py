b908795416440f96beaf1ca5650aa96f4c566adc
Diego Serafim <diegoserafim1@gmail.com>
2022-01-20 22:26:21 -0300

    err of complexity

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,15 +1,16 @@
 def find_duplicate(nums):
     duplicates = []
-    repeat = []
+    # repeat = []
     for i in nums:
         if i not in duplicates:
             duplicates.append(i)
-    for j in duplicates:
-        if duplicates.count(j) > 1:
-            if j not in repeat:
-                repeat.append(i)
+    # for j in duplicates:
+    #     if duplicates.count(j) > 1:
+    #         if j not in repeat:
+    #             repeat.append(i)
     # repeat = {duplicates.count(item) for item in duplicates}
     if type(nums) != int or nums < 1:
         return False
-
-    return repeat[0]
+        
+    print(duplicates[0] == 2)
+    return duplicates
