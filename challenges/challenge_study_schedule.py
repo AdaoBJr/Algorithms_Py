41e4cd96fec2f88655c469019696f458dfbbb39f
André Arnoni <andre_arnoni@yahoo.com.br>
2022-01-20 23:49:06 -0300

    executado desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    students_number = 0
+    if type(target_time) != int:
+        return None
+    for tuple in permanence_period:
+        if type(tuple[0]) != int or type(tuple[1]) != int:
+            return None
+        if tuple[0] <= target_time <= tuple[1]:
+            students_number += 1
+    return students_number
