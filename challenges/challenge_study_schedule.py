cc2adc895dc331d9cad92d843720dd83c11e7f7e
Fernando <fhomaia@gmail.com>
2022-01-21 20:26:30 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    counter = 0
+    for time_record in permanence_period:
+        if None in time_record or target_time is None:
+            return None
+        if time_record[0] <= target_time and time_record[1] >= target_time:
+            counter += 1
+    return counter
