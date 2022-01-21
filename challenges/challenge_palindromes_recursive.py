0bb11c6a0706f1c83923a5a6582b31f740676da9
Pedro Toselli <phtoselli@gmail.com>
2022-01-21 18:32:36 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word ,low_index + 1, high_index - 1)
+
+
+if __name__ == '__main__':
+    print(is_palindrome_recursive('SOCOS', 0, 4))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,5 @@
 def study_schedule(permanence_period, target_time):
-    student = 0
+    online_student = 0
 
     if target_time is None:
         return None
@@ -8,8 +8,5 @@ def study_schedule(permanence_period, target_time):
         if type(inicio) != int or type(fim) != int:
             return None
         if inicio <= target_time <= fim:
-            student += 1
-    return student
-
-    # print(f"inicio: {inicio}, fim: {fim}")
-    # if target_time in range(inicio, fim):
+            online_student += 1
+    return online_student
