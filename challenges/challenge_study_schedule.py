fa209186b5e60be673e3aac8bcabe2dc869dd986
antonioarieiro <sfgfffs@gmail.com>
2022-01-21 13:53:45 -0300

    fix errs lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,8 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     if(type(target_time) != int):
         return None
-    count = 0;
-    
+    count = 0
+
     for peooples in permanence_period:
         if type(peooples) is not int:
             return None
