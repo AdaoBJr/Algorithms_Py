013eec384985525b50b599e6243a79cf2eab7ab2
Diego Serafim <diegoserafim1@gmail.com>
2022-01-20 22:17:33 -0300

    challenge duplicate not completed

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,19 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    duplicates = []
+    repeat = []
+    for i in nums:
+        if i not in duplicates:
+            duplicates.append(i)
+
+    for j in duplicates:
+        if duplicates.count(j) > 1:
+            if j not in repeat:
+                repeat.append(i)
+
+    # repeat = {duplicates.count(item) for item in duplicates}
+
+    if type(nums) != int or nums < 1:
+        return False
+
+    # print(repeat)
+    return repeat
