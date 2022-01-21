4d0fab1b015f4c4f48b9b7efdb3d66d838c1adde
Misael Bueno <misa.hbueno@gmail.com>
2022-01-21 16:14:50 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
+def reverse(word):
+    if len(word) < 2:
+        return word
+    else:
+        return reverse(word[1:]) + word[0]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    word_reverse = reverse(word)
+
+    if (word_reverse == word):
+        return True
+    else:
+        return False
