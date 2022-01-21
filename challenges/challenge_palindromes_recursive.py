42172d0f72480e17e1915d53bcf967e8bec820b3
brunamfguimaraes <brunamfguimaraes@gmail.com>
2022-01-21 13:25:36 -0300

    Corrigindo erro no Flake

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,6 +9,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
 
     if low_index < high_index + 1:
-        return is_palindrome_recursive(word, low_index +1, high_index -1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     return True
