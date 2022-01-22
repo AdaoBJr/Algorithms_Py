356b94b9db022319943ad8c4a202939644ecaff6
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2022-01-22 20:23:14 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time == 0 or target_time is None:
+        return None
+
+    for schedule in permanence_period:
+        if isinstance(schedule[0], str) or isinstance(schedule[1], str):
+            return None
+        elif schedule[0] is None or schedule[1] is None:
+            return None
+        elif schedule[0] <= target_time <= schedule[1]:
+            counter += 1
+
+    return counter
