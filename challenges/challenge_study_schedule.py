66ab56482063f1cd33ff85c056f024e7279d9c19
AdaoBJr <adaobjr@gmail.com>
2022-01-22 07:25:29 -0400

    feat: :sparkles: added Req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 # iniciando o projeto
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    qty_students = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                qty_students += 1
+        return qty_students
+    except TypeError:
+        return None
