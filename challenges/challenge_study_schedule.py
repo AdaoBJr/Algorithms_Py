e8c3768d29f8fe63c3560d77235c319654179da1
Carlos <cemargato@gmail.com>
2022-01-22 19:06:04 -0300

    acerto-de-lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,11 @@
+# Gleyson explicou sobre a descontrução dentro do for 
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    
+
     count = 0
     for (entrance, exit) in permanence_period:
-        if not entrance:
-            return None
-        if not exit:
+        if entrance is None or exit is None:
             return None
         if entrance <= target_time <= exit:
             count += 1
