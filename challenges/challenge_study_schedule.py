c6dcdc1fef1faa695d3ca6ddffb1a80061de6e3b
esionascimento <esio.392@protonmail.com>
2022-01-22 14:48:04 -0400

    req 1 and 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == None:
+        return
+
+    count = 0
+
+    for i in permanence_period:
+        if type(i[0]) == int and type(i[1]) == int:
+            if i[0] <= target_time and i[1] >= target_time:
+                count += 1
+        else:
+            return
+    return count
