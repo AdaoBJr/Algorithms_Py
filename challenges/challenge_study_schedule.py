f59a5e455adf09df8269deb5e867402c77d35d2b
Felippe017 <felippecorrea2010@gmail.com>
2022-01-22 16:09:49 -0300

    Iniciando projeto: requisito 1 em andamento

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+from tokenize import Number
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == '':
+        return None
+
+    a = target_time
+    for time in permanence_period: 
+       if (type(time[0]) and type(time[1])) !=  int:
+           return None
+
+study_schedule([(1, 5), (2, 4), (3, 3), (4, 4), (5, 5)], 3)
