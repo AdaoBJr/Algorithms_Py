35c0b2dfac325d8631f97cb3bdb8c043cb8db407
Alan T <alanctnk@gmail.com>
2022-01-22 16:42:55 -0300

    req 1 ok

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
-def study_schedule(permanence_period, target_time):
-    """INICIANDO"""
+def study_schedule(permanence_period, target_time=None):
+    counter = 0
+    if target_time is None:
+        return None
+    else:
+        for (arrival_time, departure_time) in permanence_period:
+            if (type(arrival_time) is not int) or (
+                type(departure_time) is not int
+            ):
+                counter = None
+                break
+            elif arrival_time <= target_time <= departure_time:
+                counter += 1
+    return counter
