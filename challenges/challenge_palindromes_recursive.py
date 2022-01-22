ae037fb89e43ee53ba2e9d686ae4279a499031b6
Felippe017 <felippecorrea2010@gmail.com>
2022-01-22 18:05:48 -0300

    corrige lint req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -8,10 +8,10 @@ def is_palindrome_recursive(word, low_index, high_index):
     if (low_index == high_index):
         return True
 
-    if (word[low_index] != word[high_index]) :
+    if (word[low_index] != word[high_index]):
         return False
 
-    if (low_index < high_index + 1) :
+    if (low_index < high_index + 1):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    
+
     return True
