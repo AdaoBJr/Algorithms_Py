86987f2ef7501cdef58af5c2a2f210e1c2c33644
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-01-22 13:37:09 -0300

    feat(challenges): study_schedule implementation

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    pass
+    if target_time is None:
+        return None
+
+    amount = 0
+    for student_permanence_hours in permanence_period:
+        checkIn, CheckOut = (
+            student_permanence_hours[0],
+            student_permanence_hours[1],
+        )
+        if type(checkIn) is not int or type(CheckOut) is not int:
+            return None
+        if checkIn <= target_time <= CheckOut:
+            amount += 1
+
+    return amount
