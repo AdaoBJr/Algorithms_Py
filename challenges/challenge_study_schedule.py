88e03aae43c6f3b185631459d158e3581b07c2d4
Daniel <darkdanielr@gmail.com>
2022-01-22 14:11:19 -0300

    req1 corrigido2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     contador = 0
     if not isinstance(target_time, int):
         return None
-        # isinstance retorna true se for um número inteiro, nesse caso 
+        # isinstance retorna true se for um número inteiro, nesse caso
         # verifica se deu falso https://www.w3schools.com/python/ref_func_isinstance.asp
     for people in permanence_period:
         if not isinstance(people[0], int) or not isinstance(people[1], int):
