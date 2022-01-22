322382abae17b291c0c5985b15bd45dd96841ce8
Felippe017 <felippecorrea2010@gmail.com>
2022-01-22 17:32:42 -0300

    Funçao concluida, falta  passar

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,13 @@
-from tokenize import Number
-
-
 def study_schedule(permanence_period, target_time):
-    if target_time == '':
+    if not isinstance(target_time, int):
         return None
 
-    a = target_time
+    result = 0
+
     for time in permanence_period: 
-       if (type(time[0]) and type(time[1])) !=  int:
+        if (type(time[0]) and type(time[1])) !=  int:
            return None
 
-study_schedule([(1, 5), (2, 4), (3, 3), (4, 4), (5, 5)], 3)
+        if (time[0] <= target_time) and (time[1] >= target_time):
+            result += 1
+    return result
