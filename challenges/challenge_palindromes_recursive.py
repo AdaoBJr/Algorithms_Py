c79fe28e9a64e446290dabf5d72ee682070c10dd
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-01-22 15:40:46 -0300

    feat(challenges): is_palindrome_recursive implementation

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
+# Agradecimentos ao colega Pedro Henrique Pires pela elucidação na lógica
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    elif len(word) <= 2:
+        return True
+    else:
+        return is_palindrome_recursive(word[1:-1], 0, -1)
