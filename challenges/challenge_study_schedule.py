c3abdd21e74fc7cdb128d2c6a4c3b00906c0bb4f
Felippe017 <felippecorrea2010@gmail.com>
2022-01-22 17:37:28 -0300

    corrigindo lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,9 +4,9 @@ def study_schedule(permanence_period, target_time):
 
     result = 0
 
-    for time in permanence_period: 
-        if (type(time[0]) and type(time[1])) !=  int:
-           return None
+    for time in permanence_period:
+        if (type(time[0]) and type(time[1])) != int:
+            return None
 
         if (time[0] <= target_time) and (time[1] >= target_time):
             result += 1
