ed1aa8ca85ffaba811ac215ef4ac1437dc07a750
esionascimento <esio.392@protonmail.com>
2022-01-22 17:01:05 -0400

    fix req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,24 +1,23 @@
-""" A ord()função retorna um inteiro representando o caractere Unicode. """
-
-CHARS = 256
-
 def is_anagram(first_string, second_string):
+    """ A ord()função retorna um inteiro representando o caractere Unicode. """
     if first_string == "":
         return False
 
     if(len(first_string) != len(second_string)):
         return False
 
-    count1 = [0] * CHARS
-    count2 = [0] * CHARS
+    count1 = [0] * 256
+    count2 = [0] * 256
+    count = 0
 
     for i in first_string:
+        count += 1
         count1[ord(i)] += 1
 
     for i in second_string:
         count2[ord(i)] += 1
 
-    for i in range(CHARS):
+    for i in range(256):
         if count1[i] != count2[i]:
             return False
 
