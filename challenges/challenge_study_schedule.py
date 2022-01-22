f25861f02f1662a29f4c07b7fbf90dcdea68ed4a
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-22 18:19:21 -0300

    Requisito 5

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    position = len(permanence_period) // 2
+
+    if(type(target_time) is not int or permanence_period[position] is not int):
+        return None
+
+    if(target_time < permanence_period[position]):
+        newList = permanence_period[position:]
+    else:
+        newList = permanence_period[position:]
+
+    for item in newList:
+        #print(type(item[0]) is int)
+        if(type(item[0]) is not int or type(item[1]) is not int):
+            return None
+        if(item[0] <= target_time <= item[1]):
+            count += 1
+
+    return count
