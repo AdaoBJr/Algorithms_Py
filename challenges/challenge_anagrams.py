fbc4bbb90990d84c79925a6e9515ece7cd53bf07
Carlos <cemargato@gmail.com>
2022-01-22 19:24:52 -0300

    acerto-de-lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
+from contextlib import redirect_stderr
+
+from black import first_leaf_column
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+
+    first_string = split(first_string)
+
+print(a)
