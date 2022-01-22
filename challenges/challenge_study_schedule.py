6ab396b387509d4b24c1e85b3a09ea34b6e106e1
Lara Capila <laracapilak@gmail.com>
2022-01-22 19:43:13 -0300

    feat(requisito 1): Passando dois subrequisitos

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    result = 0
+
+    # não existe null em python, none representa null
+    if target_time is None:
+        return None
+
+    for input_data, output_data in permanence_period:
+        if input_data is None or output_data is None:
+            return None
+        if input_data <= target_time <= output_data:
+            result += 1
+        return result
