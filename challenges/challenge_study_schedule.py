1c4b840a7c66c2a524e491fa54de713c80d49f19
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-23 05:47:25 -0300

    Req: 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    # To sabendo rsrs \[T]/
+    totalRightTime = 0
+
+    for entryTime, departureTime in permanence_period:
+        if entryTime is None or departureTime is None:
+            return None
+        if entryTime <= target_time <= departureTime:
+            totalRightTime += 1
+
+    return totalRightTime
