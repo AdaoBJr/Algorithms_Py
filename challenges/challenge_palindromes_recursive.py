da3db760e4ceea743a27683591c9df48f46d9e7a
albertassi88 <albertassi88@gmail.com>
2022-01-23 14:06:12 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if len(word) <= 2 and word[0] == word[-1]:
+        return True
+    else:
+        return is_palindrome_recursive(
+            word[1:-1], low_index, high_index
+        )
