941c15628d3fc878ba3ba45101f9dba2d4311447
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2022-01-23 17:45:04 -0300

    Requisito 4 parcial

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,31 @@
+def binary_search_iterative(array, element):
+    mid = 0
+    start = 0
+    end = len(array)
+    step = 0
+
+    while (start <= end):
+        step = step + 1
+        mid = (start + end) // 2
+
+        if element == array[mid]:
+            return mid
+
+        if element < array[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return - 1
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    try:
+        nums_sorted = sorted(nums)
+        for index, item in enumerate(nums_sorted):
+            new_array = nums_sorted.pop(index)
+            if type(item) is not int:
+                return False
+            return binary_search_iterative(new_array, item)
+    except TypeError:
+        return False
