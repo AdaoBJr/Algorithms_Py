f0f7e97642984a24416b1817f1b962d49cc9b324
Jefferson_Andrade <jffrsncrls@gmail.com>
2022-01-23 10:37:43 -0300

    reque 2 feito

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    valor_a = list(first_string)
+    a = "".join(sorted(valor_a))
+    valor_b = list(second_string)
+    b = "".join(sorted(valor_b))
+    return a == b
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students = list()
+    if type(target_time) != int:
+        return None
+    for value in permanence_period:
+        if type(value[0]) != int or type(value[1]) != int:
+            return None
+        if value[0] <= target_time <= value[1]:
+            students.append(value)
+    return len(students)
