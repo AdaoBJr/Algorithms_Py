484eee9aa23f18fd0efb2b6d3b43594309fa6700
Fabio Marturano <fabiocmjor@gmail.com>
2022-01-23 05:48:05 -0300

    Fix: Req: 1 - nulable TargetTime

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,9 @@
 def study_schedule(permanence_period, target_time):
     totalRightTime = 0
 
+    if target_time is None:
+        return None
+
     for entryTime, departureTime in permanence_period:
         if entryTime is None or departureTime is None:
             return None
