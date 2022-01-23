e82c9852047a3d22e7deb0d0a5a431569cd140e4
Roger-Franco <rogerfrancogalt@gmail.com>
2022-01-22 23:13:40 -0400

    Primeiro commit

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    quantityStudents = 0
+    if target_time is None:
+        return None
+    for entrada, saida in permanence_period:
+        if entrada is None:
+            return None
+        if saida is None:
+            return None
+        if entrada <= target_time <= saida:
+            quantityStudents += 1
+    return quantityStudents
+    # Na função o parãmentro permanence-period vão as tuplas do range de horarios 
+    # e no target_time o horario a se testar a quantidade de alunos
