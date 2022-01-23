d71731447602227f7e76e7a89ce637e91347bdaa
Aremtior <marcosleandrofl@gmail.com>
2022-01-23 20:22:39 -0300

    Requirement 5 - is_palindrome_iterative function was created

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
+from operator import truediv
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word is None or word == '':
+        return False
+    for i in range(0, len(word) // 2):
+        if word[i] != word[len(word) - i - 1]:
+            return False
+    return True
