539d40a8aa221583c72be30987b64e9d3571f273
Jefferson_Andrade <jffrsncrls@gmail.com>
2022-01-23 09:43:27 -0300

    Done: Reque2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    elif word[low_index] != word[high_index]:
+        return False
+    elif len(word) <= 2:
+        return True
+    else:
+        valor = word[1:][:-1]
+        return is_palindrome_recursive(valor, 0, len(valor) - 1)
+
