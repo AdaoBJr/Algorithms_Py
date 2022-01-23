f7dab6e70efc2cf3ea26a685b4982ecdc1e72fa8
Felippe017 <felippecorrea2010@gmail.com>
2022-01-23 20:50:56 -0300

    3/4 req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,32 @@
+from string import ascii_lowercase as letters
+
 def is_anagram(first_string, second_string):
     if first_string == '' or second_string == '':
         return False
+    
+    def primes(n):
+        return 43142746595714191 + 5283234035979900*n
+    
+    def letter_to_prime(letter):
+        return primes(letters.index(letter))
+
+    def word_to_prod(word):
+        if len(word) == 1:
+            return letter_to_prime(word)
+        return word_to_prod(word[0]) * word_to_prod(word[1:])
+    
+    if word_to_prod(first_string) == word_to_prod(second_string):
+        return True
+    return False
+    """ first = list(first_string)
+    second = list(second_string)
+    is_true = False
+
+    for f in first:
+        if f in second:
+            is_true = True
+    
+    return is_true """
+
+
+is_anagram('coxinha', 'empudu')
