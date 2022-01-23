adf6520baf80e7b09aa11c248d84e56ef11cbd3e
Fernando <fhomaia@gmail.com>
2022-01-23 11:35:15 -0300

    refactoring req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,4 +4,22 @@ def is_anagram(first_string, second_string):
         return True
     if len(first_string) != len(second_string):
         return False
-    return False
+    char_map_one = [0]*26
+    char_map_two = [0]*26
+
+    for index in range(len(first_string)):
+        position = ord(first_string[index]) - ord('a')
+        char_map_one[position] += 1
+
+    for index in range(len(second_string)):
+        position = ord(second_string[index]) - ord('a')
+        char_map_two[position] += 1
+
+    for index in range(26):
+        if char_map_one[index] != char_map_two[index]:
+            return False
+    return True
+
+# Para a resolução deste exercício usei como referência as soluções
+# encontradas no endereço
+# https://panda.ime.usp.br/panda/static/pythonds_pt/02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
