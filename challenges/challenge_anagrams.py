e5a5953a87ab9c9a747e4032cdc62846ab0e21bd
Carlos <cemargato@gmail.com>
2022-01-22 21:10:07 -0300

    requisito-3-with-time-fail

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,35 @@
+from array import array
 from contextlib import redirect_stderr
 
 from black import first_leaf_column
 
 
+def bubble_sort(array):
+    size = len(array)-1
+    for position in range(size, 0, -1):
+        for i in range(position):
+            if array[i] > array[i+1]:
+                temporary = array[i]
+                array[i] = array[i+1]
+                array[i+1] = temporary
+    
+    return array 
+
+# https://flexiple.com/convert-string-to-list-in-python/
+
 def is_anagram(first_string, second_string):
+    if first_string is None or second_string is None:
+        return False
 
-    first_string = split(first_string)
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    first_string = list(first_string)
+    second_string = list(second_string)
 
-print(a)
+    first_string = bubble_sort(first_string)
+    second_string = bubble_sort(second_string)
+
+    if first_string == second_string:
+        return True
+    else:
+        return False
