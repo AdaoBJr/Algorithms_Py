7ca04414cee44ffc7f7dfcdfab169aee7f4c6467
Duda Wiltiner <eduardawiltiner@gmail.com>
2022-01-22 21:30:38 -0300

    Requisito 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,20 +1,25 @@
 def study_schedule(permanence_period, target_time):
-    count = 0
-    position = len(permanence_period) // 2
+    position = len(permanence_period) // 2 - 1
 
-    if(type(target_time) is not int or permanence_period[position] is not int):
+    if target_time and permanence_period[position][1]:
+        if target_time >= permanence_period[position][1]:
+            newList = permanence_period[(position):]
+        else:
+            newList = permanence_period[: (position + 2)]
+
+        return verifyInter(newList, target_time)
+
+    else:
         return None
 
-    if(target_time < permanence_period[position]):
-        newList = permanence_period[position:]
-    else:
-        newList = permanence_period[position:]
 
-    for item in newList:
-        #print(type(item[0]) is int)
-        if(type(item[0]) is not int or type(item[1]) is not int):
+def verifyInter(list, target):
+    count = 0
+    for item in list:
+        if item[0] or item[1]:
+            if item[0] <= target <= item[1]:
+                count += 1
+        else:
             return None
-        if(item[0] <= target_time <= item[1]):
-            count += 1
 
     return count
