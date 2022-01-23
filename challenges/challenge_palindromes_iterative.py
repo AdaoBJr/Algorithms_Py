4abe8fe258b7c78ff162fc7ac6449c87d2138a14
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-01-23 13:56:26 -0300

    req5 - corrige erros de linter

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,7 +1,7 @@
 def is_palindrome_iterative(word):
     if word == "":
         return False
-    
+
     empty_string = ''
     reversed_word = empty_string.join(reversed(word))
     if reversed_word == word:
