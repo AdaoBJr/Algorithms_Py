5a190dab2c8d5b680f2b432b70d70357dba7be7a
Aremtior <marcosleandrofl@gmail.com>
2022-01-23 19:02:26 -0300

    Requirement 2 - is_palindrome_recursive function was created

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word is None or word == '':
+        return False
+    if len(word) < 2:
+        return True
+    if len(word) == 2 and word[low_index] == word[high_index]:
+        return True
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word[1:high_index], 0, high_index - 2)
+    else:
+        return False
