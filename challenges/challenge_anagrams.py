63fdabc4879495d6d7390b8e4fe94101ea6d2fc5
Misael Bueno <misa.hbueno@gmail.com>
2022-01-23 11:42:08 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
+def order_str(first, second):
+    anagram = ''
+    for f in first:
+        for s in second:
+            if f == s:
+                anagram += s
+    return anagram
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+
+    anagram = order_str(first_string, second_string)
+
+    if first_string == anagram:
+        return True
+
+    return False
+
+# print(is_anagram('', 'pato'))
