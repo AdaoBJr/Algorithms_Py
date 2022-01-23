f5131531b19d2d53d6710d46085dc206f2c28bce
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2022-01-23 17:53:43 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    total_students = 0
+    for (entry, exit) in permanence_period:
+        if not isinstance(entry, int) or not isinstance(exit, int):
+            return None
+
+        if entry <= target_time <= exit:
+            total_students += 1
+
+    return total_students
