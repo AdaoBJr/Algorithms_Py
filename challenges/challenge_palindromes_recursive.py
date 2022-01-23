65ccc814bbb59f0263a05bfe57461283c36a4d0d
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2022-01-22 21:44:40 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
+def reverse(list):
+    if len(list) < 2:
+        return list
+    else:
+        return reverse(list[1:]) + list[0]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    reversed = reverse(word)
+    if word is None or word == "":
+        return False
+    elif len(word) < 2 and low_index == high_index:
+        return True
+    elif word == reversed:
+        return True
+    return False
+
