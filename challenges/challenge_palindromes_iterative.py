64ca6f0fdc24d9b3cee572359cd154b2f9c16a24
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-01-23 13:46:19 -0300

    req5 - refatora funcao is_palidrome_iterative para processar em menos tempo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,11 +2,12 @@ def is_palindrome_iterative(word):
     if word == "":
         return False
     
-    reversed_word = ""
-    for letter in word:
-        reversed_word = letter + reversed_word
+    empty_string = ''
+    reversed_word = empty_string.join(reversed(word))
     if reversed_word == word:
         return True
     else:
         return False
 
+# FONTE método reversed(): https://www.tutorialsteacher.com/python/reversed-method
+# FONTE método reversed(): https://www.delftstack.com/pt/howto/python/reverse-a-string-python/
