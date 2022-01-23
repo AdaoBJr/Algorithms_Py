52cdc1796ae0b5a0aaa34f30e7d1edbce3ad5da8
Aremtior <marcosleandrofl@gmail.com>
2022-01-23 17:47:44 -0300

    Requirement 1 - study_schedule function was created

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    total_students = 0
+    for (x, y) in permanence_period:
+        if type(x) != int or type(y) != int or target_time is None:
+            return None
+        if x <= target_time <= y:
+            total_students += 1
+    return total_students
