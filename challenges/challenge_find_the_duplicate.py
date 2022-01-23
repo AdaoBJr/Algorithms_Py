7f75295a44ea821d3fc2034fcff48485bc6f9075
Thiago Kachinsky <thiagokachidev@gmail.com>
2022-01-23 19:38:54 -0300

    reqs completed

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for item in nums:
+        if not isinstance(item, int):
+            return False
+        if item < 0:
+            return False
+        if nums.count(item) >= 2:
+            return item
+    return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not isinstance(target_time, int):
+        return None
+    count = 0
+    for element in permanence_period:
+        if (
+            not isinstance(element[0], int)
+            or not isinstance(element[1], int)
+        ):
+            return None
+        if element[0] <= target_time <= element[1]:
+            count += 1
+
+    return count
