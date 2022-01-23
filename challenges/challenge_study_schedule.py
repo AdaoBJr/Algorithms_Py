cb0a8a633e2c086b20639bb228a4d3ec8e47a5bf
Maiara Borsatti <maiara.borsatti@gmail.com>
2022-01-23 15:40:26 -0300

    requisito 1 - correçoes

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,32 +1,23 @@
-def study_schedule(permanence_period, target_time):
-    left_pointer = 0
-    right_pointer = len(permanence_period) - 1
-    all_period = []
-    start_period = permanence_period[left_pointer][0]
-    exit_period = permanence_period[left_pointer][1]
-
+""" def target(target_time):
     if type(target_time) != int or target_time is None:
-        return None
+        return None """
 
-    if type(start_period) != int or type(exit_period) != int:
-        return None
 
-    if start_period is None or exit_period is None:
-        return None
+def study_schedule(permanence_period, target_time):
+    try:
+        left_pointer = 0
+        right_pointer = len(permanence_period) - 1
+        all_period = []
 
-    while left_pointer <= right_pointer:
-        period = permanence_period[left_pointer]
-        # exit_period = permanence_period[left_pointer][1]
-        difference = period[1] - period[0]
-        """ if type(period[0]) != int or type(period[1]) != int:
-            return None
-        elif period[0] is None or period[1] is None:
-            return None """
-        if difference != 0:
-            all_period.extend(range(period[0], period[1] + 1))
-            # exit_period -= 1
+        while left_pointer <= right_pointer:
+            period = permanence_period[left_pointer]
+            if period[1] - period[0] != 0:
+                all_period.extend(range(period[0], period[1] + 1))
+            else:
+                all_period.append(period[0])
+
             left_pointer += 1
-        else:
-            all_period.append(period[0])
-            left_pointer += 1
-    return all_period.count(target_time)
+
+        return all_period.count(target_time)
+    except TypeError:
+        return None
