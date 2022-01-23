c5fe3c84353c4098db66f2f86729671894393f34
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2022-01-23 20:38:08 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not len(word):
+        return False
+
+    if len(word) == 1 or high_index < low_index:
+        return True
+
+    if low_index != high_index:
+        letters_matched = word[low_index] == word[high_index]
+
+        # Verificção de palavras com tamanho par
+        if len(word) == 2:
+            return letters_matched
+
+        return letters_matched and is_palindrome_recursive(word[1:high_index], low_index, high_index-2)
