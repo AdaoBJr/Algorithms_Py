11eee25c188e17d6327d9f5ba9f0ad3bd9273b75
Maiara Borsatti <maiara.borsatti@gmail.com>
2022-01-23 17:47:54 -0300

    requisito 3 - correçao

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,17 @@
+# Dica de Fernanda Porto
+# https://stackoverflow.com/questions/13101468/python-how-to-sort-the-alphabet-in-a-list-without-sorted-functions
+def quicksort(string):
+    if not string:
+        return []
+    return (
+        quicksort([x for x in string[1:] if x < string[0]])
+        + [string[0]]
+        + quicksort([x for x in string[1:] if x >= string[0]])
+    )
+
+
 # https://github.com/tryber/sd-10b-live-lectures/blob/lecture/35.3/35_3_ordenacao_e_busca/1-progressao.py
-def insertion_sort(array):
+""" def insertion_sort(array):
     for i in range(1, len(array)):
         current_value = array[i]
         current_position = i - 1
@@ -12,15 +24,12 @@ def insertion_sort(array):
 
         array[current_position + 1] = current_value
 
-    return array
+    return array """
 
 
 def is_anagram(first_string, second_string):
-    first = insertion_sort(list(first_string))
-    second = insertion_sort(list(second_string))
-
-    # print(first)
-    # print(second)
+    first = quicksort(first_string)
+    second = quicksort(second_string)
 
     if first == "" or second == "":
         return False
@@ -29,6 +38,3 @@ def is_anagram(first_string, second_string):
         return False
 
     return True
-
-
-# is_anagram("pedra", "perda")
