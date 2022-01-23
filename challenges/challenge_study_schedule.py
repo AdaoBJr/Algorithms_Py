379e21a81aaba7cbcbc3dc676d3ee8c70e1c9a84
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2022-01-23 12:48:37 -0300

    Last refacs on Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,8 +5,6 @@ def study_schedule(permanence_period, target_time):
             for tupla in permanence_period
             if tupla[0] <= target_time <= tupla[1]
         ]
-        if time_validation != []:
-            counter = len(time_validation)
+        return len(time_validation)
     except TypeError:
         return None
-    return counter
