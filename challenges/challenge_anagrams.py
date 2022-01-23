4d047bb8bb9704cfebc0a9b5927de5e235199c73
Fernando <fhomaia@gmail.com>
2022-01-23 10:48:48 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,11 +4,4 @@ def is_anagram(first_string, second_string):
         return True
     if len(first_string) != len(second_string):
         return False
-    for first in first_string:
-        check = False
-        for second in second_string:
-            if first == second:
-                check = True
-        if not check:
-            return False
-    return True
+    return False
