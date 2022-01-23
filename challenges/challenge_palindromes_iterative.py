55ed91c1479e43e3846f73928c73077d51e8dba6
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-01-23 14:05:46 -0300

    req5 - retorna ao codigo que estava anteriormente que nao passava no teste local mas passou no avaliador

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,15 +2,14 @@ def is_palindrome_iterative(word):
     if word == "":
         return False
 
-    empty_string = ''
-    reversed_word = empty_string.join(reversed(word))
+    reversed_word = ""
+    for letter in word:
+        reversed_word = letter + reversed_word
     if reversed_word == word:
         return True
     else:
         return False
 
 
-# FONTE método reversed():
-# https://www.tutorialsteacher.com/python/reversed-method
-# FONTE método reversed():
-# https://www.delftstack.com/pt/howto/python/reverse-a-string-python/
+# FONTE:
+# encurtador.com.br/gkHOV
