69a4207885c13fca6b0fa2e2965bd4514a440829
Antonio Arieiro jr <sfgfffs@gmail.com>
2022-01-24 19:01:50 -0300

    (iterative) if word = word reverse  == palindrome

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,4 +1,11 @@
+# https://stackoverflow.com/questions/31633635/what-is-the-meaning-of-inta-1-in-python
+
+
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    reverse = word[::-1]
     if(not word):
         return False
+
+    if word == reverse:
+        return True
