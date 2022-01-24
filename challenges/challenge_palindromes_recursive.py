33dcafeef540123dd64a57b3f7c6ffdfee2a3543
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-01-24 19:03:36 -0300

    Fix error flake8

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,15 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if(len(word) == 0):
+    if len(word) == 0:
         return False
-    
     if word[low_index] != word[high_index]:
         return False
-    
-    if (low_index == high_index):
+    if low_index == high_index:
         return True
-    
-    if(low_index < high_index + 1):
+    if low_index < high_index + 1:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-        
     return True
-
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,9 +4,8 @@ def study_schedule(permanence_period, target_time):
 
     response = 0
     for period in permanence_period:
-        if(None in period):
+        if None in period:
             return None
-        
-        if(period[0]<= target_time <= period[1]):
+        if period[0] <= target_time <= period[1]:
             response += 1
-    return response 
+    return response
