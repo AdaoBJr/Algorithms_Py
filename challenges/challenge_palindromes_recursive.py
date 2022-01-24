309b807cbee1dd422e2649fb8a311e0be49753e1
Roger-Franco <rogerfrancogalt@gmail.com>
2022-01-23 22:43:33 -0400

    Req02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if word == "":
+        return False
+    if word[0] != word[-1]:
+        return False
+    if len(word) == 1:
+        return True
+    if len(word) == 2 and word[0] == word[1]:
+        return True
+    return is_palindrome_recursive(word[1:-1], low_index, high_index)
