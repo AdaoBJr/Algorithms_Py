027331da4ab228c18cc4a123110773fb4398b870
Rafael <rmathscoach@gmail.com>
2022-01-23 22:04:09 -0300

    partial requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+    
+    def is_anagram_recursive(first_string, second_string, first_index, second_index):
+        if first_index == len(first_string) or second_index == len(second_string):
+            return True
+        if first_string[first_index] != second_string[second_index]:
+            return False
+        if(first_index < len(first_string) and second_index < len(second_string)):
+            return is_anagram_recursive(first_string, second_string, first_index + 1, second_index + 1)
+        return True
+    
+    return is_anagram_recursive(first_string, second_string, 0, 0)
+  
