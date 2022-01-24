f563803cc2fbb477a312e74a1ba7d5b31b6ca02c
joao966 <joaooliveira966@gmail.com>
2022-01-23 21:49:43 -0300

    ADD: request1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        list_best_time = [
+            (in_, out)
+            for in_, out in permanence_period
+            if in_ <= target_time <= out
+        ]
+
+        return len(list_best_time)
+    except TypeError:
+        return None
