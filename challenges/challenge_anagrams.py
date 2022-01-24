a07a926c8d44f58d3c61e9cb0b29a9d9f8ddecc3
Pedro Toselli <phtoselli@gmail.com>
2022-01-24 14:01:32 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,23 @@
+def sort_string(string):
+    string_list = list(string)
+
+    for i in range(1, len(string)):
+        if string_list[i] < string_list[i - 1]:
+            string_list[i], string_list[i - 1] = string_list[i - 1] , string_list[i]
+
+    return ''.join(string_list)
+
+
 def is_anagram(first_string, second_string):
-    pass
+    str1 = sort_string(first_string)
+    str2 = sort_string(second_string)
+
+    print(str1, str2)
+
+    if str1 == str2:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    print(is_anagram('amor', 'roma'))
