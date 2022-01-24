c32a5b90b3df6f6f014143887dd04058010c3d77
Emerson Saturnino <emerson.saturnino@finnet.com.br>
2022-01-24 20:27:53 -0300

    3 - Anagrams (Sort Algorithm)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return True
+
+    if first_string[0] in second_string:
+        string = second_string.replace(first_string[0], "", 1)
+        return is_anagram(first_string[1:], string)
+    else:
+        return False
