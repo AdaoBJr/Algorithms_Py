d22b4a1c78a6abe6b62ed808894a22100a8e8971
Felippe017 <felippecorrea2010@gmail.com>
2022-01-23 21:56:26 -0300

    refatorando codigo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,6 +2,8 @@ from challenges.helper_anagram import letter_to_prime
 
 
 def is_anagram(first_string, second_string):
+    # obtive ajuda do site 
+    # https://medium.com/@omadson/um-algoritmo-inteligente-para-descobrir-se-duas-palavras-s%C3%A3o-ou-n%C3%A3o-anagramas-9f4a108a63e
     if first_string == '' or second_string == '':
         return False
 
