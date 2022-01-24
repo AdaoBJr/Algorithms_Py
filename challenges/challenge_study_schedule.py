7aae6ec686a7208214021a3074607aeb273a5db0
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-01-24 20:29:32 -0300

    second attempt

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     for period in permanence_period:
-        if None in period:
+        if None in (period[0], period[1]):
             return None
         if period[0] <= target_time <= period[1]:
             response += 1
