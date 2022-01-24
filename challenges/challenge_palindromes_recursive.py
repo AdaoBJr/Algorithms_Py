f861a49e766ee5609cc982445e990a22cd5880f1
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2022-01-24 09:33:22 -0300

    lint correction

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -12,4 +12,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         if len(word) == 2:
             return letters_matched
 
-        return letters_matched and is_palindrome_recursive(word[1:high_index], low_index, high_index-2)
+        return letters_matched \
+            and is_palindrome_recursive(
+                word[1:high_index], low_index, high_index-2)
