5d4374933fea7013677624ba783c3e59778e960e
Emerson Saturnino <emerson.saturnino@finnet.com.br>
2022-01-24 19:58:59 -0300

    feat/Requirement 1 - Number of students studying in the system

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    total_students = 0
+    for system_login, system_logout in permanence_period:
+        if None in (system_login, system_logout):
+            return None
+        if system_login <= target_time <= system_logout:
+            total_students += 1
+    return total_students
