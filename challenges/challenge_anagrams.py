ec9ee2bae92e0450be61eeafe97b1a3b436770fc
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-01-24 20:58:38 -0300

    Done attempt of req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
+from challenges.helper_is_anagram import is_anagram_recursive
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if second_string == "" or first_string == "":
+        return False
+    return is_anagram_recursive(first_string, second_string, 0, 0)
---
+++ challenges/helper_is_anagram.py
@@ -0,0 +1,13 @@
+def is_anagram_recursive(fst_string, sec_string, fst_index, sec_index):
+    if fst_index == len(fst_string) or sec_index == len(sec_string):
+        return True
+    if fst_string[fst_index] != sec_string[sec_index]:
+        return False
+    if fst_index < len(fst_string) and sec_index < len(sec_string):
+        return is_anagram_recursive(
+            fst_string, sec_string, fst_index + 1, sec_index + 1
+        )
+    return True
+
+
+print(is_anagram_recursive("roma", "amor", 0, 3))
