80d018148fbbd1ff5b6207c65c76d1af7fa790c9
Emerson Saturnino <emerson.saturnino@finnet.com.br>
2022-01-24 20:33:11 -0300

    fix/3 - Anagrams (Sort Algorithm)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,6 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
-        return True
+        return False
 
     if first_string[0] in second_string:
         string = second_string.replace(first_string[0], "", 1)
