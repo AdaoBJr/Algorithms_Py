1f2020712d792f41d772da90fe9a492a8c9b8839
Felippe017 <felippecorrea2010@gmail.com>
2022-01-23 21:34:45 -0300

    done req 3

--- challenges/helper_anagram.py
+++ challenges/helper_anagram.py
@@ -1,7 +1,9 @@
 from string import ascii_lowercase as letters
 
+
 def primes(n):
-        return 43142746595714191 + 5283234035979900*n
+    return 43142746595714191 + 5283234035979900*n
+
 
 def letter_to_prime(letter):
     return primes(letters.find(letter))
