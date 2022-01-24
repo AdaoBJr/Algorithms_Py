778c07657b179c2ccbe33758547a078afb9a2f6d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2022-01-24 19:19:03 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
+def sort_string(string):
+    array_char = [char for char in string]
+    for i in range(len(array_char)):
+        min = i
+
+        for j in range(i + 1, len(array_char)):
+            if array_char[j] < array_char[min]:
+                min = j
+
+        array_char[min], array_char[i] = array_char[i], array_char[min]
+
+    return ''.join(array_char)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not len(first_string) or not len(second_string):
+        return False
+    return sort_string(first_string) == sort_string(second_string)
