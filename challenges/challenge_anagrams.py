5ace02c8e273863d69622f369e2f838466dc1a5a
João Herculano de Souza Neto <joaohsneto@gmail.com>
2022-01-23 22:01:15 -0300

    Resolvido o requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+from challenges.helper_anagram import letter_to_prime
+# https://medium.com/@omadson/um-algoritmo-inteligente-para-descobrir-se-duas-palavras-s%C3%A3o-ou-n%C3%A3o-anagramas-9f4a108a63e
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+
+    def word_to_prod(word):
+        if len(word) == 1:
+            return letter_to_prime(word)
+        return word_to_prod(word[0]) * word_to_prod(word[1:])
+
+    if word_to_prod(first_string) == word_to_prod(second_string):
+        return True
+    return False
