a74e573c4b412419144347d8c67c79f06c52a8be
Nícolas Tozato <ntozato9@gmail.com>
2022-01-24 16:40:59 -0300

    implementa study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
+    counter = 0
+
+    if target_time is None:
+        return None
     
+    for student in permanence_period:
+        if student[0] is None or student[1] is None:
+            return None
+        if student[0] <= target_time <= student[1]:
+            counter += 1
+
+    return counter
+
+
+teste = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+print(study_schedule(teste, None))
