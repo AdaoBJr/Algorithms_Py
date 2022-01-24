c66dc03392436a2cb8d7574ba2ec0d49628a141e
Rafael <rmathscoach@gmail.com>
2022-01-23 21:05:34 -0300

    requirement 1: DONE

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    counter_students = 0
+    for entry_time, exit_time in permanence_period:
+        if None in (entry_time, exit_time):
+            return None
+        if entry_time <= target_time <= exit_time:
+            counter_students += 1
+    return counter_students
