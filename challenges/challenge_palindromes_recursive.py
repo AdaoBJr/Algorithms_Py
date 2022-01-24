943c7740dabb9b73e694692de4fa7d4be1f5c2ae
Rafael <rmathscoach@gmail.com>
2022-01-23 21:52:13 -0300

    requirement2: DONE

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
+# referencia: https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if low_index == high_index:
+        return True
+    if word == '':
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if(low_index < high_index):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
