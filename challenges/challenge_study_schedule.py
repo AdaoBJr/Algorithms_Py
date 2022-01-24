40c0357fe55dbf1efcd6c415ba077ca5f1ab220d
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2022-01-24 00:09:46 -0300

    Done requirement 02

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    response = 0
+    for period in permanence_period:
+        if(None in period):
+            return None
+        
+        if(period[0]<= target_time <= period[1]):
+            response += 1
+    return response 
