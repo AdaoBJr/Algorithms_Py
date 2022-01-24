89fd42cc013789572ed232e714e3fb0292b62b5c
Rafael <rmathscoach@gmail.com>
2022-01-23 23:06:43 -0300

    fix flake

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     counter_students = 0
-    for time  in permanence_period:
+    for time in permanence_period:
         if None in (time[0], time[1]):
             return None
         if time[0] <= target_time <= time[1]:
