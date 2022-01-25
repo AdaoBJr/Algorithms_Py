dbe78a79c0417bcde37314f56ffe85e532550b50
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-25 15:51:27 -0500

    Starting a new project... Help me GOD

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
+from asyncio.windows_events import NULL
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time < 0 or target_time is NULL:
+        return None
+    occurrencies = 0
+    for schedule in permanence_period:
+        if schedule[0] >= target_time or schedule[1] <= target_time:
+            occurrencies += 1
+    return occurrencies
