767ce0951d809644e5aac98734436f1704b6e371
Nícolas Tozato <ntozato9@gmail.com>
2022-01-24 21:15:38 -0300

    implementa is_palindrome_recursive

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -13,5 +13,5 @@ def study_schedule(permanence_period, target_time):
     return counter
 
 
-teste = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-print(study_schedule(teste, None))
+# teste = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(teste, None))
