053c7f6e1d59d9a2f29be128a1fb0413386dd9df
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-01-25 11:59:38 -0300

    req1 - reprovando no teste do tempo localmente. Envia para o avaliador para verificar se passa

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,25 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word == "":
+        return False
+    if len(word) < 2:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index > high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    
+    
+    
+    
+    
+    # if not word or word == "":
+    #     return False
+    # if len(word) < 2:
+    #     return True
+    # if word[low_index] != word[high_index]:
+    #     return False
+    # return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+#FONTE: encurtador.com.br/kHNTY
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    # Verifica se o horário passado (target_time) é um número
+    if type(target_time) is not int:
+        return None
+
+    students = 0
+    for permanence in permanence_period:
+        # Verifica se a hora de entrada (permanence[0]) e a hora de saída (permanence[1]) são inteiros
+        if type(permanence[0]) is not int or type(permanence[1]) is not int:
+            return None
+        # Verifica se o horário passado (target_time) está entre os horários de entrada e saída. Caso esteja, siginifica que existe pelo menos um estudante naquele horário, então ele acrescenta esse estudante à variável 'students'
+        if permanence[0] <= target_time <= permanence[1]:
+            students = students + 1
+    return students
