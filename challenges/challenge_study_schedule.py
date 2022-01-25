6ac719e1c54f9e59a92e9bed3e33891f466427a5
Renan Braga <renan.braga@clina.care>
2022-01-25 19:12:38 -0200

    adiciona req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+#  estudante             1       2       3       4       5       6
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    result = 0
+
+    if not target_time:
+        return None
+    
+    for period in permanence_period:
+        if None in period:
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            result += 1
+    return result
+
