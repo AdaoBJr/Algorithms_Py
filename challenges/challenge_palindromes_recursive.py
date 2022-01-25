302a712fd3a3b21a294ad68c6e1f0e805964ce3c
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-01-25 18:54:26 -0300

    req2 - implementa funçao is_palidrome_recursive. Passando localmente, subindo para verificar se passa no avaliador

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça seu código aqui"""
+    # verifica se word não é uma string vazia
+    if not word:
+        return False
+    # verifica se o último elemento da string é igual ao primeiro...
+    # Caso sejam, esta palavra PODE ser um palíndromo.
+    if word[low_index] != word[high_index]:
+        return False
+    # verifica se low_index é maior ou igual o high_index. (Caso Base)
+    if low_index >= high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+# FONTE: encurtador.com.br/ehuSV
+# Comparecimento ao plantão onde João me judou na formulação do caso base
