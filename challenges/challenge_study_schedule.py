ea4a6fbee9c27f64236f5cd930ec7dadd03116b1
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-25 18:58:30 -0300

    Inserir - challenge_study_schedule.py - Estruturação inicial do função que procura o melhor horario afim de coletar maior audiencia para executar uma ação

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) is not int:
+        return None
+    count = 0
+    for student in permanence_period:
+        if type(student) is not int:
+            return None
+        if student[0] < target_time < student[1]:
+            count += 1
+    return count
