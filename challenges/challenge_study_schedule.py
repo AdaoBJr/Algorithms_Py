ff103137d3f9db0c29ed65518e052b273a5cedc3
Gildeir <gildeirlopes@gmail.com>
2022-01-24 22:26:54 -0300

    Adding challenges palindromes_recursive and study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    restul = 0
+    for start in permanence_period:
+        if start[0] is None or start[1] is None:
+            return None
+        if start[0] <= target_time <= start[1]:
+            restul += 1
+    return restul
+
