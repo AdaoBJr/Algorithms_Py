1b0bdbae35a670328cad6486d5d16b21c042dec8
Camila Arruda <camilaam1802@gmail.com>
2022-01-25 14:59:31 -0300

    Teste do requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+from tkinter import N
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    #Retorne None se em target_time houver alguma entrada inválida
+    if(target_time is None): return None # = null
+
+    contador = 0;
+    for (entrada, saida) in permanence_period:
+        if entrada is None or saida is None: return None;
+        #Entrada tem que ser <= time e saida tem que ser >= time
+        if (entrada <= target_time) and (saida >= target_time):
+            contador += 1
+    return contador;         
+
+
+
