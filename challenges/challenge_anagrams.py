4ef2eab97392e54a43b3057339f423d8f9f14fd1
Gildeir <gildeirlopes@gmail.com>
2022-01-25 20:06:28 -0300

    adding challenge_anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,23 @@
+def merged(value):
+
+    arrString = []
+    primary_string = list(value)
+    while value:
+        half = primary_string[0]
+        for range in primary_string:
+            if range < half:
+                half = range
+        arrString.append(half)
+        if len(primary_string) != 1:
+            primary_string.remove(half)
+        else:
+            break
+    return ''.join(arrString)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = merged(first_string)
+    second = merged(second_string)
+    if first == second:
+        return True
+    return False
