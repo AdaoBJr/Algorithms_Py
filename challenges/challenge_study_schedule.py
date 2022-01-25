aa6e3873af3a3adc25fe6364f1b4f75a836ff27e
Camila Arruda <camilaam1802@gmail.com>
2022-01-25 15:04:28 -0300

    corrigindo erros de Lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,15 @@
-from tkinter import N
-
 
 def study_schedule(permanence_period, target_time):
-    #Retorne None se em target_time houver alguma entrada inválida
-    if(target_time is None): return None # = null
+    # Retorne None se em target_time houver alguma entrada inválida
+    if(target_time is None):
+        return None
+        # = null
 
-    contador = 0;
+    contador = 0
     for (entrada, saida) in permanence_period:
-        if entrada is None or saida is None: return None;
-        #Entrada tem que ser <= time e saida tem que ser >= time
+        if entrada is None or saida is None:
+            return None
+        # Entrada tem que ser <= time e saida tem que ser >= time
         if (entrada <= target_time) and (saida >= target_time):
             contador += 1
-    return contador;         
-
-
-
+    return contador
