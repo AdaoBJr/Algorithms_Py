b04e7c408a030cb0663bb78c708abf8317740ace
Dângelo Silva Miranda <dangelo.silva.miranda@gmail.com>
2022-01-26 03:14:04 -0300

    feat: :sparkles: req 02 complete

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word[low_index] != word[high_index]:
+        return False
+
+    if low_index < high_index:
+        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return True
+
+
+# if __name__ == "__main__":
+#     word = "AGUA"
+#     print(f"{is_palindrome_recursive(word, 0, len(word) - 1)}")
