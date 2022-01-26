bb286ffb2f4f290de0c75d95b9a6e401269d321f
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-01-26 16:09:27 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    counter = 0
     try:
+        counter = 0
         for entrance, exit in permanence_period:
             if entrance <= target_time <= exit:
                 counter += 1
     except TypeError:
         return None
-    # if target_time is None:
-    #     return None
     return counter
 
 
