b1fccf59e6f0c2407876a96cf00bbd4c45d05f7c
Dângelo Silva Miranda <dangelo.silva.miranda@gmail.com>
2022-01-26 04:10:41 -0300

    fix: :bug: fix req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,7 +3,8 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
 
     if low_index < high_index:
-        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        # print(f"{word[low_index]} == {word[high_index]}")
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     return True
 
