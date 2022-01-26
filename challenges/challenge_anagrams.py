d19840e64e30675ad5826f05d1f7da6ae67de67e
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-26 11:46:07 -0500

    Requirement three

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,27 +1,11 @@
-def merge(left, right, merged):
-    left_cursor, right_cursor = 0, 0
-    while left_cursor < len(left) and right_cursor < len(right):
-        if left[left_cursor] < right[right_cursor]:
-            merged[left_cursor + right_cursor] = left[left_cursor]
-            left_cursor += 1
-        else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
-            right_cursor += 1
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left_cursor
-    for right_cursor in range(right_cursor, len(right)):
-        merged[left_cursor + right_cursor] = right_cursor
-    return merged
+# https://stackoverflow.com/questions/13101468/python-how-to-sort-the-alphabet-in-a-list-without-sorted-functions
 
-
-def merge_sort(word):
-    letter_list = list(word)
-    if len(word) <= 1:
-        return word
-    middle = len(word) // 2
-    left, right = merge_sort(
-        letter_list[:middle]), merge_sort(letter_list[middle:])
-    return merge(left, right, letter_list)
+def quicksort(string):
+    if not string:
+        return []
+    return (quicksort([x for x in string[1:] if x < string[0]])
+            + [string[0]] +
+            quicksort([x for x in string[1:] if x >= string[0]]))
 
 
 def is_anagram(first_string, second_string):
@@ -29,9 +13,8 @@ def is_anagram(first_string, second_string):
         len(first_string) != len(second_string)
             or first_string == "" or second_string == ""):
         return False
-    sorted_string_one = merge_sort(first_string)
-    sorted_string_two = merge_sort(second_string)
-
+    sorted_string_one = quicksort(first_string)
+    sorted_string_two = quicksort(second_string)
     if sorted_string_one == sorted_string_two:
         return True
     else:
