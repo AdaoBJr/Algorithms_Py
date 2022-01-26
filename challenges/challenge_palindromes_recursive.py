5a2fe62493a5923184047feacb28bac2ce5a3b3d
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-26 20:19:22 -0300

    Inserir - challenge_palindromes_recursive.py - Criação da estrutura inicial da função que verifica quando uma palavra é palindroma ou não

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    finish_function = (high_index + 1) - (low_index + 1)
+    if len(word) < 1 or not word:
+        return False
+    if finish_function > 1:
+        if word[low_index] == word[high_index]:
+            is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return False
+    return True
