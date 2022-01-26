e31e64a7140e50f1bc17070273ae60b544bba184
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2022-01-26 09:48:59 -0500

    Correction of the validations

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
-from asyncio.windows_events import NULL
-
 
 def study_schedule(permanence_period, target_time):
-    if target_time < 0 or target_time is NULL:
+    if not target_time:
         return None
     occurrencies = 0
     for schedule in permanence_period:
-        if schedule[0] >= target_time or schedule[1] <= target_time:
+        if None in schedule:
+            return None
+        if schedule[0] <= target_time and schedule[1] >= target_time:
             occurrencies += 1
     return occurrencies
