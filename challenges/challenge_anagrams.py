1fd5385123f345dca29b8ea4759cd9ad682daf3a
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-26 20:50:07 -0300

    Inserir - Correção do flake8

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,4 @@
 def is_anagram(first_string, second_string):
     if len(first_string) == 0 or len(second_string) == 0:
         return False
-    start = 0
-    end = len(first_string)
-    mid = start + end // 2
     return True
