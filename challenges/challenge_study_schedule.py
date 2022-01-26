8798ff2e187f109ef24d9c9b86e7b8a156074193
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-26 19:01:51 -0300

    Inserir - challenge_study_schedule.py - Feito o requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,8 +3,8 @@ def study_schedule(permanence_period, target_time):
         return None
     count = 0
     for student in permanence_period:
-        if type(student) is not int:
+        if type(student[0]) is not int or type(student[1]) is not int:
             return None
-        if student[0] < target_time < student[1]:
+        if student[0] <= target_time <= student[1]:
             count += 1
     return count
