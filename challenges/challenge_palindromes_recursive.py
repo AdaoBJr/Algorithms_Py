547a7ec8182867c9f6901612bcd2248b269a1538
Joao Vanelli <vanellijoao@gmail.com>
2022-01-26 18:40:30 -0300

    Req 2: Palindrome Recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if word[high_index] != word[low_index]:
+        return False
+
+    if high_index > low_index:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return True
