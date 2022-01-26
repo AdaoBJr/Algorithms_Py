99104f8943c37b39688c4d1144ee038f95072bbd
Leandro Reis <lleandrofr@gmail.com>
2022-01-26 19:45:55 -0300

    feat(study_schedule): return how many students are within the time interval

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,16 @@
 def study_schedule(permanence_period, target_time):
+    students = 0
     if type(target_time) != int:
         return None
 
     for (start, end) in permanence_period:
         if type(start) != int or type(end) != int:
             return None
+
+        if start <= target_time <= end:
+            students += 1
+
+    return students
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 3))
