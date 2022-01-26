c6a1455f520a9b04ea51c26a9b5dadf4fa3dce2b
v-icto-r <victor130496@gmail.com>
2022-01-26 20:24:31 -0300

    requisito 3(parte)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    # feito com ajuda da Letycia barreto
+    if first_string != second_string:
+        return False
+    if first_string == "" or second_string == "":
+        return False
+    if set(first_string.lower()) == set(second_string.lower()):
+        return True
