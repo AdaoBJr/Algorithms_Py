ca121c4e8acd0bafecfb7c40fbcf1545e10cd016
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-01-26 17:56:39 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def sorting(string):
+    newArray = list(string)
+    output = []
+    while newArray:
+        lower_letter = newArray[0]
+        for letter in newArray:
+            if letter < lower_letter:
+                lower_letter = letter
+        output.append(lower_letter)
+        newArray.remove(lower_letter)
+    return output
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    return sorting(first_string) == sorting(second_string)
