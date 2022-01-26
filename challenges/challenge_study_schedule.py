ab3ce649172d2c7122227f17f5a051036f79a341
Joao Vanelli <vanellijoao@gmail.com>
2022-01-26 20:31:06 -0300

    Req 1: Study Schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    people_present = 0
+
+    if target_time is None:
+        return None
+
+    for (enter_time, exit_time) in permanence_period:
+        if type(enter_time) is not int or type(exit_time) is not int:
+            return None
+
+        if enter_time <= target_time <= exit_time:
+            people_present += 1
+
+    return people_present
