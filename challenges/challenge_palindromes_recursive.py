f0b0f4b917075f0cc52c7831529eb3ae36317807
Leandro Reis <lleandrofr@gmail.com>
2022-01-26 20:32:00 -0300

    feat(palindrome): check if a word is a palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    if word[low_index] == word[high_index]:
+        if len(word) == 2:
+            return True
+
+        sliced = word[1:-1]
+
+        return is_palindrome_recursive(sliced, 0, len(sliced) - 1)
+
+    return False
+
+
+# word = "GG"
+# is_palindrome_recursive("gg", 0, len(word) - 1)
