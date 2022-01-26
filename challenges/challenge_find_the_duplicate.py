76a21cff6090cd82ce7e66085d1b61af461ca7bb
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2022-01-26 18:26:05 -0300

    requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if len(nums) <= 1:
+        return False
+
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+        repeated_list = [num for num in nums if nums.count(num) > 1]
+        if len(repeated_list) > 0:
+            return repeated_list[0]
+        else:
+            return False
