26a4a452447d1af26c17e7475b4ed6eef48f4ca9
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-26 20:21:59 -0300

    Inserir - challenge_palindromes_recursive.py - Feito o requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    finish_function = (high_index + 1) - (low_index + 1)
+    finish_function = high_index - low_index
     if len(word) < 1 or not word:
         return False
     if finish_function > 1:
         if word[low_index] == word[high_index]:
-            is_palindrome_recursive(word, low_index + 1, high_index - 1)
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
         return False
+    if finish_function == 1:
+        if word[low_index] != word[high_index]:
+            return False
     return True
