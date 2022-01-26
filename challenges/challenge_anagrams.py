4d1d5fe0d056c5ab6be97d00dc4f7d4bbcea9fef
André Arnoni <andre_arnoni@yahoo.com.br>
2022-01-26 20:37:07 -0300

    executado requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
+def verify(string):
+    original = list(string)
+    new = []
+
+    while string:
+        ob = original[0]
+        for i in original:
+            if i < ob:
+                ob = i
+        new.append(ob)
+        if len(original) != 1:
+            original.remove(ob)
+        else:
+            break
+    return ''.join(new)
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui.. """
+    first = verify(first_string)
+    second = verify(second_string)
+
+    if first == second:
+        return True
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,8 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if low_index >= high_index:
         return True
-    if word[low_index] != word[high_index]:
+    elif word[low_index] != word[high_index]:
         return False
-    if not word:
-        return False
-    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
