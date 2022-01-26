715a415b0b67815263aa908c79dde0e3329d820e
Jonathan Souza <souzajonathan570@gmail.com>
2022-01-26 20:13:44 -0300

    adiciona o merge_sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,22 +1,41 @@
-def insertion_sort(string):
+def merge_sort(string):
     letter_list = list(string)
     length = len(letter_list)
 
-    for index in range(1, length):
-        i = index
+    if length <= 1:
+        return letter_list
 
-        while (i > 0 and letter_list[i] < letter_list[i - 1]):
-            aux = letter_list[i]
-            letter_list[i] = letter_list[i - 1]
-            letter_list[i - 1] = aux
-            i -= 1
+    mid = length // 2
 
-    return letter_list;
+    list_left, list_right = merge_sort(letter_list[:mid]), merge_sort(letter_list[mid:])
+
+    return merge(list_left, list_right, letter_list.copy())
+
+
+def merge(left, right, merged):
+    index_left, index_right = 0, 0
+
+    while (index_left < len(left) and index_right < len(right)):
+
+        if left[index_left] <= right[index_right]:
+            merged[index_left + index_right] = left[index_left]
+            index_left += 1
+        else:
+            merged[index_left + index_right] = right[index_right]
+            index_right += 1
+
+    for index_left in range(index_left, len(left)):
+        merged[index_left + index_right] = left[index_left]
+
+    for index_right in range(index_right, len(right)):
+        merged[index_left + index_right] = right[index_right]
+
+    return merged
 
 
 def is_anagram(first_string, second_string):
-    first_list = insertion_sort(first_string)
-    second_list = insertion_sort(second_string)
+    first_list = merge_sort(first_string)
+    second_list = merge_sort(second_string)
     
     if first_list == second_list:
         return True
