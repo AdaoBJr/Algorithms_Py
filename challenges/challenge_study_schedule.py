b8f0c97c94b20ff8ccc271b1c76d2daea4100b4b
Bruno Wedley <brunowedley27@gmail.com>
2022-01-26 14:08:12 -0300

    adiciona primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    number_of_students = 0
+    if target_time is None:
+        return None
+    for period in permanence_period:
+        if None in period:
+            return None
+        if period[0] <= target_time <= period[1]:
+            number_of_students +=1
+    return number_of_students
