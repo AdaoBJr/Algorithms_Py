18602a0a5e76efcc2b8f510104c608f70991c675
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-26 20:46:39 -0300

    Inserir - challenge_palindromes_recursive.py - Refatoração na função, retirada de ifs que não eram necessarios

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 0 or not word:
+        return False
+    if word[::-1] == word:
+        return True
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     finish_function = high_index - low_index
-    if len(word) < 1 or not word:
+    if len(word) < 1:
         return False
-    if finish_function > 1:
+    if finish_function > 0:
         if word[low_index] == word[high_index]:
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
         return False
-    if finish_function == 1:
-        if word[low_index] != word[high_index]:
-            return False
     return True
