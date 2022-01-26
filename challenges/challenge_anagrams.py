53388ecffbbe630ae16472adc19f491e0384391b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-01-26 20:49:29 -0300

    Inserir - challenge_anagrams.py - Criação da estrutura inicial da função que verifica palavras anagramaticas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,7 @@
 def is_anagram(first_string, second_string):
+    if len(first_string) == 0 or len(second_string) == 0:
+        return False
     start = 0
     end = len(first_string)
     mid = start + end // 2
-    pass
+    return True
