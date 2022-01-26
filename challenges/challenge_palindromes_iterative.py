d2f2b1a5158debe11da5b38d49d8b32bc19354fe
Joao Vanelli <vanellijoao@gmail.com>
2022-01-26 18:49:18 -0300

    Req 5: fix Palindrome Iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,6 @@
 def is_palindrome_iterative(word):
+    if not word:
+        return False
+
     # https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
     return word == word[::-1]
