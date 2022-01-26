2ffd2430c14ed9ac037ade8afba320e6f31b91de
André Arnoni <andre_arnoni@yahoo.com.br>
2022-01-25 22:54:51 -0300

    executado requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if not word:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
